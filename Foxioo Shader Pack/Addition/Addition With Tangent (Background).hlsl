12dc2226ee6f2591f02e9c1ae13b45fa9a91ef75
Foxioo <foxioo2137@gmail.com>
2024-06-23 15:57:05 +0200

    Adding missing shaders

---
+++ Foxioo Shader Pack/Addition/Addition With Tangent (Background).hlsl
@@ -0,0 +1,59 @@
+/***********************************************************/
+
+/* Autor shader: Foxioo */
+/* Version shader: 1.1 (23.06.2024) */
+/* My GitHub: https://github.com/FoxiooOfficial */
+
+/***********************************************************/
+
+/* ####################################################### */
+
+/***********************************************************/
+/* Samplers */
+/***********************************************************/
+Texture2D<float4> S2D_Image : register(t0);
+SamplerState S2D_ImageSampler : register(s0);
+
+Texture2D<float4> S2D_Background : register(t1);
+SamplerState S2D_BackgroundSampler : register(s1);
+
+/***********************************************************/
+/* Varibles */
+/***********************************************************/
+
+cbuffer PS_VARIABLES : register(b0)
+{
+    bool _;
+    float _Mixing;
+    bool __;
+};
+
+struct PS_INPUT
+{
+  float4 Tint : COLOR0;
+  float2 texCoord : TEXCOORD0;
+};
+
+struct PS_OUTPUT
+{
+    float4 Color   : SV_TARGET;
+};
+
+/************************************************************/
+/* Main */
+/************************************************************/
+
+PS_OUTPUT ps_main( in PS_INPUT In )
+{
+    PS_OUTPUT Out;
+
+    float4 _Render_Texture = S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint;
+    float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.texCoord);
+
+    float4 _Result = tan(_Render_Texture + (_Render_Background * _Mixing));
+
+    _Result.a = _Render_Texture.a;
+    Out.Color = _Result;
+    
+    return Out;
+}
