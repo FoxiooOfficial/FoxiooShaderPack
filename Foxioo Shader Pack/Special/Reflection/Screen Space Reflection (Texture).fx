b87ddb7edc5e29d7d7e02d9228c7c92ec91c4305
Foxioo <foxioo2137@gmail.com>
2026-07-17 03:26:34 +0200

    fixed SSR shader

--- Foxioo Shader Pack/Special/Reflection/Screen Space Reflection (Texture).fx
+++ Foxioo Shader Pack/Special/Reflection/Screen Space Reflection (Texture).fx
@@ -1,8 +1,9 @@
 /***********************************************************/
 
-/* Shader author: Foxioo */
-/* Version shader: 1.0 (18.01.2026) */
-/* My GitHub: https://github.com/FoxiooOfficial */
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
 
 /***********************************************************/
 
@@ -31,7 +32,7 @@ sampler2D S2D_Image : register(s0);
 /* Main */
 /************************************************************/
 
-static int _MaxSteps = 12;
+static int _MaxSteps = 9;
 
 bool Fun_Comp(float3 _Color)
 {
@@ -43,33 +44,82 @@ bool Fun_Comp(float3 _Color)
 
 float4 Main(in float2 In : TEXCOORD0) : COLOR0
 {
-    float4 _Render_Texture = (tex2D(S2D_Image, In));
-    float4 _Render_Texture_Save = _Render_Texture;
-    
-    if(Fun_Comp(_Render_Texture.rgb)) return float4(0.0, 0.0, 0.0, 0.0);
+    float4 _Render_Texture = tex2D(S2D_Image, In);
+
+    if(Fun_Comp(_Render_Texture.rgb)) return 0.0;
+    else if(_Render_Texture.a >= 1.0) return _Render_Texture;
     else
     {
-        float _Rad = _Angle * 0.0174532925199444; //(3.14159265359 / 180.0);
-        float2 _Ray = float2(cos(_Rad), sin(_Rad)) * float2(fPixelWidth, fPixelHeight) * _Size;
+        /* Screenspace Reflection! */
+        float _Rad = _Angle * 0.0174532925199444; // (3.14159265359 / 180.0);
 
+        float2 _SinCos;
+        sincos(_Rad, _SinCos.x, _SinCos.y);
+
+        float2 _Ray = _SinCos * float2(fPixelWidth, fPixelHeight) * _Size;
         float2 UV = In + float2(_OffsetX, _OffsetY) * float2(fPixelWidth, fPixelHeight);
 
-        float4 _Render_Reflection = tex2D(S2D_Image, In);
-        float2 _Hit = float2(0.0, 0.0);
-        
-            for (int i = _MaxSteps; i > 1; i--)
+            float4 _Render_Reflection = tex2D(S2D_Image, UV);
+            float2 _Hit = float2(0.0, 0.0);
+
+        /* raymarching */
+        //bool _Break = false;
+        for (int i = _MaxSteps; i > 1; i--)
+        {
+            UV += _Ray * (64.0 / _MaxSteps);
+
+            //if (any(UV <= 0.0 || UV >= 1.0)) 
+            //    _Break = true;
+
+            float4 _Render_Reflected = tex2D(S2D_Image, UV);
+
+            //if (Fun_Comp(_Render_Reflected.rgb))
+            //    _Break = true;
+                
+            if (_Render_Reflected.a > _Threshold)
+            {        
+                float2 UV_Ref = In + (_Ray * float(i * (64.0 / _MaxSteps)) * _Jump);
+                float4 _Render = tex2D(S2D_Image, UV_Ref);
+                        
+                //if (any(UV_Ref <= 0.0 || UV_Ref >= 1.0) || Fun_Comp(_Render.rgb)) 
+                //    _Break = true;
+
+                _Render.rgb *= _Color.rgb;
+
+                    //if(_Render.a == 0.0)
+                    //    _Break = true;
+                    
+                    if (_Render.a > _Threshold) 
+                    {
+                        _Render_Reflection = _Render;
+                        _Hit = float2((float(i) * 2.0) / float(_MaxSteps), 1.0); 
+                    }
+                }
+        }
+
+        if (bool(_Hit.y))
+        {
+            float _InvertedFade = 1.0 - _Hit.x; 
+
+            if(_InvertedFade <= 0.0)
+                return 0.0;
+            else
             {
-                UV += _Ray;
+                _InvertedFade = saturate(pow(abs(_InvertedFade * _InvertedFade), _Fade));
 
-                float2 UV_Ref = In + (_Ray * float(i) * _Jump); 
+                float _Alpha = _Strength * _InvertedFade * _Render_Reflection.a;
+                
+                float4 _Render;
+                _Render.rgb = lerp(_Render_Texture.rgb, _Render_Reflection.rgb, _Alpha);
+                _Render.a = max(_Render_Texture.a, _Alpha); 
 
-                float4 _Render = tex2D(S2D_Image, UV_Ref) * float4(_Color.rgb, 1.0);
-                _Render_Texture = _Render;
-                _Hit = float2((float(i) * 2.0) / float(_MaxSteps), 1.0); 
+                _Render = lerp(_Render_Texture, _Render, _Mixing);
+
+                return _Render;
             }
-            //_Render_Reflection /= _MaxSteps;
-
-        return lerp(_Render_Texture_Save, lerp(_Render_Texture, tex2D(S2D_Image, In), tex2D(S2D_Image, In).a), _Mixing);
+        }
+        else
+            return _Render_Reflection;
     }
 }
 
@@ -77,4 +127,4 @@ float4 Main(in float2 In : TEXCOORD0) : COLOR0
 /* Tech Main */
 /************************************************************/
 
-technique tech_main { pass P0 { PixelShader = compile ps_2_0 Main(); } }
+technique tech_main { pass P0 { PixelShader = compile ps_2_a Main(); } }
--- Foxioo Shader Pack/Special/Reflection/Screen Space Reflection (Texture).hlsl
+++ Foxioo Shader Pack/Special/Reflection/Screen Space Reflection (Texture).hlsl
@@ -1,8 +1,9 @@
 /***********************************************************/
 
-/* Shader author: Foxioo */
-/* Version shader: 1.0 (18.01.2026) */
-/* My GitHub: https://github.com/FoxiooOfficial */
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
 
 /***********************************************************/
 
@@ -32,6 +33,7 @@ cbuffer PS_VARIABLES : register(b0)
     float _Strength;
     float _Threshold;
     float _Fade;
+    int _Loop;
     float4 _Color;
     float4 _ColorIgnore;
     float _OffsetX;
@@ -41,8 +43,9 @@ cbuffer PS_VARIABLES : register(b0)
 
 struct PS_INPUT
 {
-  float4 Tint : COLOR0;
-  float2 texCoord : TEXCOORD0;
+    float4 Tint : COLOR0;
+    float2 texCoord : TEXCOORD0;
+    float4 Position : SV_POSITION;
 };
 
 struct PS_OUTPUT
@@ -60,7 +63,7 @@ cbuffer PS_PIXELSIZE : register(b1)
 /* Main */
 /************************************************************/
 
-static int _MaxSteps = 64;
+//static int _MaxSteps = 256;
 
 bool Fun_Comp(float3 _Color)
 {
@@ -70,56 +73,75 @@ bool Fun_Comp(float3 _Color)
     return all(abs(_Color.rgb - _ColorIgnore.rgb) <= 0.01);
 }
 
-PS_OUTPUT ps_main( in PS_INPUT In )
+float4 Demultiply(float4 _Render, bool _Premultiplied)
 {
-    PS_OUTPUT Out;
+    if(_Premultiplied)
+    {
+	    if ( _Render.a != 0.0 ) {
+            _Render.rgb /= _Render.a;
+        }
+    }
 
-    float4 _Render_Texture = S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint;
+	return _Render;
+}
+
+float4 Main(in PS_INPUT In, bool _Premultiplied) : SV_TARGET
+{
+    float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint, _Premultiplied);
+
+    if(Fun_Comp(_Render_Texture.rgb))
+        return 0.0;
     
-    if(Fun_Comp(_Render_Texture.rgb)) { Out.Color = float4(0.0, 0.0, 0.0, 0.0); return Out; }
-    if (_Render_Texture.a > 0.95) { Out.Color = _Render_Texture; return Out;}
+    if (_Render_Texture.a >= 1.0)
+        return _Render_Texture;
 
     /* Screenspace Reflection! */
+        float _Rad = _Angle * 0.0174532925199444; // (3.14159265359 / 180.0);
 
-    float _Rad = _Angle * 0.0174532925199444; //(3.14159265359 / 180.0);
-    float2 _Ray = float2(cos(_Rad), sin(_Rad)) * float2(fPixelWidth, fPixelHeight) * _Size;
-    
-    float2 UV = In.texCoord + float2(_OffsetX, _OffsetY) * float2(fPixelWidth, fPixelHeight);
+        float2 _SinCos;
+        sincos(_Rad, _SinCos.x, _SinCos.y);
 
-    float4 _Render_Reflection =  S2D_Image.Sample(S2D_ImageSampler, UV) * In.Tint;
-    float2 _Hit = float2(0.0, 0.0);
+        float2 _Ray = _SinCos * float2(fPixelWidth, fPixelHeight) * _Size;
+        float2 UV = In.texCoord + float2(_OffsetX, _OffsetY) * float2(fPixelWidth, fPixelHeight);
+
+            float4 _Render_Reflection = Demultiply(S2D_Image.Sample(S2D_ImageSampler, UV) * In.Tint, _Premultiplied);
+            float2 _Hit = 0.0;
 
     /* raymarching */
-    [unroll(32)]
-    for (int i = 1; i <= _MaxSteps; i++)
-    {
-        UV += _Ray;
+    int _Steps = clamp(_Loop, 0, 1024);
 
-        if (any(UV < 0.0 || UV > 1.0)) 
+    [loop]
+    for (int i = 1; i <= _Steps; i++)
+    {
+        UV += _Ray * (64.0 / (float)_Loop);
+
+        if (any(UV <= 0.0 || UV >= 1.0)) 
             break;
 
-        float4 _Render_Reflected = S2D_Image.Sample(S2D_ImageSampler, UV) * In.Tint;
-        
+        float4 _Render_Reflected = Demultiply(S2D_Image.SampleLevel(S2D_ImageSampler, UV, 0) * In.Tint, _Premultiplied);
+
         if (Fun_Comp(_Render_Reflected.rgb))
             continue;
             
         if (_Render_Reflected.a > _Threshold)
         {        
-            float2 UV_Ref = In.texCoord + (_Ray * float(i) * _Jump);
-
-            if (all(UV >= 0.0 && UV <= 1.0)) 
-            {
-                float4 _Render = S2D_Image.Sample(S2D_ImageSampler, UV_Ref) * In.Tint;
+            float2 UV_Ref = In.texCoord + (_Ray * float(i * (64.0 / (float)_Loop)) * _Jump);
+            float4 _Render = Demultiply(S2D_Image.SampleLevel(S2D_ImageSampler, UV_Ref, 0) * In.Tint, _Premultiplied);
                     
-                _Render.rgb *= _Color.rgb;
+            if (any(UV_Ref <= 0.0 || UV_Ref >= 1.0) || Fun_Comp(_Render.rgb)) 
+                break;
+
+            _Render.rgb *= _Color.rgb;
+
+                if(_Render.a == 0.0)
+                    break;
                 
-                if (_Render.a > _Threshold && !Fun_Comp(_Render.rgb)) 
+                if (_Render.a > _Threshold) 
                 {
                     _Render_Reflection = _Render;
-                    _Hit = float2((float(i) * 2.0) / float(_MaxSteps), 1.0); 
-                    break;
+                    _Hit = float2((float(i) * 2.0) / float(_Steps), 1.0); 
                 }
-            }
+            
             break;
         }
     }
@@ -127,12 +149,13 @@ PS_OUTPUT ps_main( in PS_INPUT In )
     /* fade :3 */
     if (bool(_Hit.y))
     {
-        float __Fade = 1.0 - _Hit.x; 
-        __Fade = saturate(pow(abs(__Fade * __Fade), _Fade));
+        float _InvertedFade = 1.0 - _Hit.x; 
+        if(_InvertedFade <= 0.0)
+            return 0.0; 
 
-        _Render_Reflection *= In.Tint; 
+        _InvertedFade = saturate(pow(abs(_InvertedFade * _InvertedFade), _Fade));
 
-        float _Alpha = _Strength * __Fade * _Render_Reflection.a;
+        float _Alpha = _Strength * _InvertedFade * _Render_Reflection.a;
         
         float4 _Render;
         _Render.rgb = lerp(_Render_Texture.rgb, _Render_Reflection.rgb, _Alpha);
@@ -140,102 +163,25 @@ PS_OUTPUT ps_main( in PS_INPUT In )
 
         _Render = lerp(_Render_Texture, _Render, _Mixing);
 
-        Out.Color = _Render;
+        return _Render;
     }
     else
-        Out.Color = _Render_Texture;
-
-    return Out;
+        return _Render_Reflection;
 }
 
 /************************************************************/
-/* Premultiplied Alpha */
+/* Render */
 /************************************************************/
 
-float4 Demultiply(float4 _Color)
-{
-	if ( _Color.a != 0 )   _Color.rgb /= _Color.a;
-	return _Color;
+float4 ps_main(in PS_INPUT In) : SV_TARGET { 
+    float4 _Render = Main(In, false);
+    return _Render;
 }
 
-PS_OUTPUT ps_main_pm( in PS_INPUT In ) 
+float4 ps_main_pm(in PS_INPUT In) : SV_TARGET
 {
-    PS_OUTPUT Out;
+    float4 _Render = Main(In, true);
+    _Render.rgb *= _Render.a;
 
-    float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord)) * In.Tint;
-    
-    if(Fun_Comp(_Render_Texture.rgb)) { Out.Color = float4(0.0, 0.0, 0.0, 0.0); return Out; }
-    if (_Render_Texture.a > 0.95) { _Render_Texture.rgb *= _Render_Texture.a; Out.Color = _Render_Texture; return Out;}
-
-    /* Screenspace Reflection! */
-
-    float _Rad = _Angle * 0.0174532925199444; //(3.14159265359 / 180.0);
-    float2 _Ray = float2(cos(_Rad), sin(_Rad)) * float2(fPixelWidth, fPixelHeight) * _Size;
-    
-    float2 UV = In.texCoord + float2(_OffsetX, _OffsetY) * float2(fPixelWidth, fPixelHeight);
-
-    float4 _Render_Reflection =  Demultiply(S2D_Image.Sample(S2D_ImageSampler, UV)) * In.Tint;
-    float2 _Hit = float2(0.0, 0.0);
-
-    /* raymarching */
-    [unroll(32)]
-    for (int i = 1; i <= _MaxSteps; i++)
-    {
-        UV += _Ray;
-
-        if (any(UV < 0.0 || UV > 1.0)) 
-            break;
-
-        float4 _Render_Reflected = Demultiply(S2D_Image.Sample(S2D_ImageSampler, UV)) * In.Tint;
-        
-        if (Fun_Comp(_Render_Reflected.rgb))
-            continue;
-            
-        if (_Render_Reflected.a > _Threshold)
-        {        
-            float2 UV_Ref = In.texCoord + (_Ray * float(i) * _Jump);
-
-            if (all(UV >= 0.0 && UV <= 1.0))  
-            {
-                float4 _Render = Demultiply(S2D_Image.Sample(S2D_ImageSampler, UV_Ref)) * In.Tint;
-                    
-                _Render.rgb *= _Color.rgb;
-                
-                if (_Render.a > _Threshold && !Fun_Comp(_Render.rgb)) 
-                {
-                    _Render_Reflection = _Render;
-                    _Hit = float2((float(i) * 2.0) / float(_MaxSteps), 1.0); 
-                    break;
-                }
-            }
-            break;
-        }
-    }
-
-    /* fade :3 */
-    if (bool(_Hit.y))
-    {
-        float __Fade = 1.0 - _Hit.x; 
-        __Fade = saturate(pow(abs(__Fade * __Fade), _Fade));
-
-        _Render_Reflection *= In.Tint; 
-
-        float _Alpha = _Strength * __Fade * _Render_Reflection.a;
-        
-        float4 _Render;
-        _Render.rgb = lerp(_Render_Texture.rgb, _Render_Reflection.rgb, _Alpha);
-        _Render.a = max(_Render_Texture.a, _Alpha); 
-
-        _Render = lerp(_Render_Texture, _Render, _Mixing);
-
-        _Render.rgb *= _Render.a;
-        Out.Color = _Render;
-    }
-    else
-    {
-        _Render_Texture.rgb *= _Render_Texture.a;
-        Out.Color = _Render_Texture;
-    }
-
-    return Out;
+    return _Render;
 }
