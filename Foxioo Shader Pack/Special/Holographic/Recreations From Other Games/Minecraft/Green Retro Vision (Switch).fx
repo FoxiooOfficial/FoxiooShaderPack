ae87dfb2891812e091896662aac15c0918376f84
Foxioo <foxioo2137@gmail.com>
2026-07-18 20:00:21 +0200

    aww man

---
+++ Foxioo Shader Pack/Special/Holographic/Recreations From Other Games/Minecraft/Green Retro Vision (Switch).fx
@@ -0,0 +1,76 @@
+/***********************************************************/
+
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
+
+/***********************************************************/
+
+/* ####################################################### */
+
+/***********************************************************/
+/* Samplers */
+/***********************************************************/
+
+sampler2D S2D_Image : register(s0);
+sampler2D S2D_Background : register(s1);
+
+/***********************************************************/
+/* Varibles */
+/***********************************************************/
+
+    float   _Mixing,
+            fPixelWidth, fPixelHeight;
+
+    float4  _ColorLight, _ColorShadow;
+
+    bool    _Blending_Mode;
+
+/************************************************************/
+/* Main */
+/************************************************************/
+
+float4 Main(in float2 In : TEXCOORD0) : COLOR0
+{
+    const float _Steps = 32.0;
+    const float _Res = 4.0;
+
+    float2 _PixelSize = float2(fPixelWidth, fPixelHeight);
+    float _ResSize = _Res * _Mixing;
+
+        float4 _Render_Texture = tex2D(S2D_Image, In);
+        float4 _Render_Background = tex2D(S2D_Background, In);
+
+        float4 _Result, _Render;
+
+        float2 UV = floor(In / _PixelSize / _ResSize) * _PixelSize * _ResSize;
+
+            if(!_Blending_Mode)
+            {
+                _Result = tex2D(S2D_Image, UV);
+                _Render = _Render_Texture;
+            }
+            else
+            {
+                _Result.rgb = tex2D(S2D_Background, UV);
+                _Render = _Render_Background;
+
+                _Result.a = _Render_Texture.a;
+            }
+
+            float _Lum = dot(_Result.rgb, float3(0.299, 0.587, 0.114));
+
+                _Result.rgb = lerp(_ColorShadow.rgb, _ColorLight.rgb, _Lum) * 1.55;
+                _Result.rgb = floor(_Result.rgb * _Steps) / _Steps;
+
+            _Result = lerp(_Render, _Result, _Mixing);
+
+    return _Result;
+}
+
+/************************************************************/
+/* Tech Main */
+/************************************************************/
+
+technique tech_main { pass P0 { PixelShader = compile ps_2_a Main(); } }
---
+++ Foxioo Shader Pack/Special/Holographic/Recreations From Other Games/Minecraft/Green Retro Vision (Switch).hlsl
@@ -0,0 +1,126 @@
+/***********************************************************/
+
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
+
+/***********************************************************/
+
+/* ####################################################### */
+
+/***********************************************************/
+/* Samplers */
+/***********************************************************/
+
+Texture2D<float4> S2D_Image : register(t0);
+SamplerState S2D_ImageSampler : register(s0);
+
+Texture2D<float4> S2D_Background : register(t1);
+SamplerState S2D_BackgroundSampler : register(s1);
+
+/***********************************************************/
+/* Varibles */
+/***********************************************************/
+
+cbuffer PS_VARIABLES : register(b0)
+{
+    bool _;
+    bool _Blending_Mode;
+    float _Mixing;
+    float4 _ColorLight;
+    float4 _ColorShadow;
+	bool ___;
+    bool _Is_Pre_296_Build;
+    bool ____;
+};
+
+struct PS_INPUT
+{
+    float4 Tint : COLOR0;
+    float2 texCoord : TEXCOORD0;
+    float4 Position : SV_POSITION;
+};
+
+struct PS_OUTPUT
+{
+    float4 Color   : SV_TARGET;
+};
+
+cbuffer PS_PIXELSIZE : register(b1)
+{
+	float fPixelWidth;
+	float fPixelHeight;
+};
+
+/************************************************************/
+/* Main */
+/************************************************************/
+
+float4 Demultiply(float4 _Render, bool _Premultiplied)
+{
+    if(_Premultiplied)
+    {
+	    if ( _Render.a != 0.0 ) {
+            _Render.rgb /= _Render.a;
+        }
+    }
+
+	return _Render;
+}
+
+float4 Main(in PS_INPUT In, bool _Premultiplied) : SV_TARGET
+{
+    const float _Steps = 32.0;
+    const float _Res = 4.0;
+
+    float2 _PixelSize = float2(fPixelWidth, fPixelHeight);
+    float _ResSize = _Res * _Mixing;
+
+        float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint, _Premultiplied);
+        float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.texCoord);
+
+        float4 _Result, _Render;
+        _Render.a = _Render_Texture.a;
+
+        float2 UV = floor(In.texCoord / _PixelSize / _ResSize) * _PixelSize * _ResSize;
+
+            if(!_Blending_Mode)
+            {
+                _Result = Demultiply(S2D_Image.Sample(S2D_ImageSampler, UV) * In.Tint, _Premultiplied);
+                _Render = _Render_Texture;
+            }
+            else
+            {
+                _Result.rgb = S2D_Background.Sample(S2D_BackgroundSampler, UV).rgb;
+                _Render = _Render_Background;
+
+                _Result.a = _Render_Texture.a;
+            }
+
+            float _Lum = dot(_Result.rgb, float3(0.299, 0.587, 0.114));
+
+                _Result.rgb = lerp(_ColorShadow.rgb, _ColorLight.rgb, _Lum) * 1.55;
+                _Result.rgb = floor(_Result.rgb * _Steps) / _Steps;
+
+            _Result = lerp(_Render, _Result, _Mixing);
+
+    return _Result;
+}
+
+/************************************************************/
+/* Render */
+/************************************************************/
+
+float4 ps_main(in PS_INPUT In) : SV_TARGET { 
+    float4 _Render = Main(In, false);
+    return _Render;
+}
+
+float4 ps_main_pm(in PS_INPUT In) : SV_TARGET
+{
+    float4 _Render = Main(In, true);
+    _Render.rgb *= _Render.a;
+
+    return _Render;
+}
