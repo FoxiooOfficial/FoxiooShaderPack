22e0dd249f7370a98ffabaeb917fcfad3f53a92f
Foxioo <foxioo2137@gmail.com>
2026-08-02 19:17:54 +0200

    updated "fake" active shader

--- Foxioo Shader Pack/Special/Detail/Active (Texture).fx
+++ Foxioo Shader Pack/Special/Detail/Active (Texture).fx
@@ -1,8 +1,9 @@
 /***********************************************************/
 
-/* Shader author: Foxioo */
-/* Version shader: 1.1 (18.10.2025) */
-/* My GitHub: https://github.com/FoxiooOfficial */
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
 
 /***********************************************************/
 
@@ -12,12 +13,7 @@
 /* Samplers */
 /***********************************************************/
 
-sampler2D S2D_Image : register(s0) = sampler_state
-{
-    AddressU = BORDER;
-    AddressV = BORDER;
-    BorderColor = float4(0, 0, 0, 0);
-};
+sampler2D S2D_Image : register(s0);
 //sampler2D S2D_Background : register(s1);
 
 /***********************************************************/
@@ -25,29 +21,41 @@ sampler2D S2D_Image : register(s0) = sampler_state
 /***********************************************************/
 
     float   _Mixing,
-            _Sharpness_Size,
+            _Size,
+            _Angle,
+            _Threshold,
 
             fPixelWidth, fPixelHeight;
 
-    //bool    _Blending_Mode;
+    float4 _ColorEx, _Color;
 
 /************************************************************/
 /* Main */
 /************************************************************/
 
-float2 Fun_Border(float2 In, float2 _Size, float _Alpha)
+float Fun_Luminance(float4 _Result)
 {
-    _Size *= float2(fPixelWidth, fPixelHeight) * _Sharpness_Size;
+    const float _Kr = 0.299;
+    const float _Kg = 0.587;
+    const float _Kb = 0.114;
 
+    float _Y = saturate(0.1 + (_Kr * _Result.r + _Kg * _Result.g + _Kb * _Result.b));
+
+    return saturate(step(_Y * _Result.a, _Threshold));
+}
+
+float2 Fun_Sharp(sampler2D _Sampler, float2 In, float2 _Off, float _Alpha)
+{
     float2 _Result;
+
     _Result.x = (
-        tex2D(S2D_Image, (In + _Size) ).a * 2.0 +
-        tex2D(S2D_Image, (In - _Size) ).a * 2.0 
+        Fun_Luminance(tex2D(_Sampler, In + _Off)) * 2.0 +
+        Fun_Luminance(tex2D(_Sampler, In - _Off)) * 2.0 
     ) - _Alpha * 2.0;
 
     _Result.y = (
-        tex2D(S2D_Image, (In + _Size) ).a * 2.0 +
-        tex2D(S2D_Image, (In - _Size) ).a / 2.0 
+        Fun_Luminance(tex2D(_Sampler, In + _Off)) * 2.0 +
+        Fun_Luminance(tex2D(_Sampler, In - _Off)) / 2.0 
     ) - _Alpha;
 
     return _Result;
@@ -56,52 +64,66 @@ float2 Fun_Border(float2 In, float2 _Size, float _Alpha)
 float4 Main(in float2 In : TEXCOORD0) : COLOR0
 {
     float4 _Render_Texture = tex2D(S2D_Image, In);
-    //float4 _Render_Background = tex2D(S2D_Background, In);
+    
+        float4 _Result;
+        float2 _Render;
+        float2 _Offset = _Size * -float2(fPixelWidth, fPixelHeight);
 
-    float4 _Result;
-    float2 _Result_Sharpness;
+        float _Sin;
+        float _Cos;
+        sincos(radians(_Angle), _Sin, _Cos);
+        _Offset = float2(
+            _Offset.x * _Cos - _Offset.y * _Sin,
+            _Offset.x * _Sin + _Offset.y * _Cos
+        );
 
-        //_Result_Sharpness = Fun_Border(In, 3.5, _Render_Texture.a);
-        _Result.rgb = float3(0.0, 0.5, 0.5);// - _Result_Sharpness.x * 0.3999;
-        
-        /* ############################# */
-        _Result_Sharpness = Fun_Border(In, 5., _Render_Texture.a);
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 * (_Result_Sharpness.y + 0.1), saturate((0.25 - saturate(abs(_Result_Sharpness.x * 6.0))) * 0.2));
+            _Result.rgb = _Color.rgb;
+            // float3(184.0, 219.0, 219.0)
 
-        _Result_Sharpness = Fun_Border(In, float2(-5., 5.), _Render_Texture.a);
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 * (_Result_Sharpness.y), saturate((0.25 - saturate(abs(_Result_Sharpness.x * 6.0))) * 0.2));
-        
-        /* ############################# */
+            /* ############################# */
 
-        /* ############################# */
-        _Result_Sharpness = Fun_Border(In, 4.95, _Render_Texture.a);
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 * (_Result_Sharpness.y + 0.1), saturate((0.25 - saturate(abs(_Result_Sharpness.x * 6.0))) * 1.5));
+            _Render = Fun_Sharp(S2D_Image, In, 5.0 * _Offset, _Render_Texture.a);
+            _Result.rgb = lerp(_Result.rgb, _ColorEx.rgb * (_Render.y + 0.1), saturate((0.25 - saturate(abs(_Render.x * 6.0))) * 0.2));
 
-        float2 _Result_Side_L = _Result_Sharpness;
-        _Result_Sharpness = Fun_Border(In, float2(-4.95, 4.95), _Render_Texture.a);
-        float2 _Result_Side_R = _Result_Sharpness;
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 * (_Result_Sharpness.y), saturate((0.25 - saturate(abs(_Result_Sharpness.x * 6.0))) * 0.5));
-        
-        /* ############################# */
+            _Render = Fun_Sharp(S2D_Image, In, float2(-5.0, 5.0) * _Offset, _Render_Texture.a);
+            _Result.rgb = lerp(_Result.rgb, _ColorEx.rgb * _Render.y, saturate((0.25 - saturate(abs(_Render.x * 6.0))) * 0.2));
+            
+            /* ############################# */
 
-        /* ############################# */
-        _Result_Sharpness = Fun_Border(In, 2.0, _Render_Texture.a) * 0.3;
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 + min(_Result_Sharpness.y - 0.09, 0.0), saturate(0.5 - saturate(abs(_Result_Sharpness.x * 6.0))));
+            /* ############################# */
 
-        _Result_Sharpness = Fun_Border(In, float2(-2.0, 2.0), _Render_Texture.a) * 0.3;
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 + min(_Result_Sharpness.y - 0.09, 0.0),  saturate(0.5 - saturate(abs(_Result_Sharpness.x * 6.0))));
-        /* ############################# */
+            _Render = Fun_Sharp(S2D_Image, In, 4.95 * _Offset, _Render_Texture.a);
+            _Result.rgb = lerp(_Result.rgb, _ColorEx.rgb * (_Render.y + 0.1), saturate((0.25 - saturate(abs(_Render.x * 6.0))) * 1.5));
 
-        /* ############################# */
-        _Result_Sharpness = Fun_Border(In, 1.85, _Render_Texture.a) * 0.3;
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 + min(_Result_Sharpness.y - 0.09, 0.0) + _Result_Side_L.y * 0.1, saturate((1.0 - saturate(abs(_Result_Sharpness.x * 6.0))) - (_Result_Side_L.y * 0.4)));
+            float2 _Result_Side_L = _Render;
+            _Render = Fun_Sharp(S2D_Image, In, float2(-4.95, 4.95) * _Offset, _Render_Texture.a);
+            _Result.rgb = lerp(_Result.rgb, _ColorEx.rgb * _Render.y, saturate((0.25 - saturate(abs(_Render.x * 6.0))) * 0.5));
+            
+            /* ############################# */
 
-        _Result_Sharpness = Fun_Border(In, float2(-1.85, 1.85), _Render_Texture.a) * 0.3;
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 + min(_Result_Sharpness.y - 0.09, 0.0), 1.0 - saturate(abs(_Result_Sharpness.x * 6.0)));
-        /* ############################# */
+            /* ############################# */
+
+            _Render = Fun_Sharp(S2D_Image, In, 2.0 * _Offset, _Render_Texture.a) * 0.3;
+            _Result.rgb = lerp(_Result.rgb, _ColorEx.rgb + min(_Render.y - 0.09, 0.0), saturate(0.5 - saturate(abs(_Render.x * 6.0))));
+
+            _Render = Fun_Sharp(S2D_Image, In, float2(-2.0, 2.0) * _Offset, _Render_Texture.a) * 0.3;
+            _Result.rgb = lerp(_Result.rgb, _ColorEx.rgb + min(_Render.y - 0.09, 0.0),  saturate(0.5 - saturate(abs(_Render.x * 6.0))));
+            
+            /* ############################# */
+
+            /* ############################# */
+
+            _Render = Fun_Sharp(S2D_Image, In, 1.85 * _Offset, _Render_Texture.a) * 0.3;
+            _Result.rgb = lerp(_Result.rgb, _ColorEx.rgb + min(_Render.y - 0.09, 0.0) + _Result_Side_L.y * 0.1, saturate((1.0 - saturate(abs(_Render.x * 6.0))) - (_Result_Side_L.y * 0.4)));
+
+            _Render = Fun_Sharp(S2D_Image, In, float2(-1.85, 1.85) * _Offset, _Render_Texture.a) * 0.3;
+            _Result.rgb = lerp(_Result.rgb, _ColorEx.rgb + min(_Render.y - 0.09, 0.0), 1.0 - saturate(abs(_Render.x * 6.0)));
+            
+            /* ############################# */
 
         _Result.rgb = lerp(_Render_Texture.rgb, _Result.rgb, _Mixing);
-        _Result.a =  _Render_Texture.a;
+        _Result.a = _Render_Texture.a;
+
     return _Result;
 }
 
--- Foxioo Shader Pack/Special/Detail/Active (Texture).hlsl
+++ Foxioo Shader Pack/Special/Detail/Active (Texture).hlsl
@@ -1,8 +1,9 @@
 /***********************************************************/
 
-/* Shader author: Foxioo */
-/* Version shader: 1.1 (18.10.2025) */
-/* My GitHub: https://github.com/FoxiooOfficial */
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
 
 /***********************************************************/
 
@@ -26,7 +27,11 @@ cbuffer PS_VARIABLES : register(b0)
 {
     bool _;
     float _Mixing;
-    float _Sharpness_Size;
+    float _Size;
+    float _Angle;
+    float _Threshold;
+    float4 _Color;
+    float4 _ColorEx;
     bool __;
 };
 
@@ -51,144 +56,125 @@ cbuffer PS_PIXELSIZE : register(b1)
 /* Main */
 /************************************************************/
 
-float2 Fun_Border(float2 In, float2 _Size, float _Alpha)
+float4 Demultiply(float4 _Render, bool _Premultiplied)
 {
-    _Size *= float2(fPixelWidth, fPixelHeight) * _Sharpness_Size;
+    if(_Premultiplied)
+    {
+	    if ( _Render.a != 0.0 ) {
+            _Render.rgb /= _Render.a;
+        }
+    }
 
+	return _Render;
+}
+
+float Fun_Luminance(float4 _Result)
+{
+    const float _Kr = 0.299;
+    const float _Kg = 0.587;
+    const float _Kb = 0.114;
+
+    float _Y = saturate(0.1 + (_Kr * _Result.r + _Kg * _Result.g + _Kb * _Result.b));
+
+    return saturate(step(_Y * _Result.a, _Threshold));
+}
+
+float2 Fun_Border(Texture2D _Texture, SamplerState _Sampler, float2 In, float2 _Off, float _Alpha)
+{
     float2 _Result;
+
     _Result.x = (
-        S2D_Image.Sample(S2D_ImageSampler, (In + _Size) ).a * 2.0 +
-        S2D_Image.Sample(S2D_ImageSampler, (In - _Size) ).a * 2.0 
+        Fun_Luminance(_Texture.Sample(_Sampler, In + _Off)) * 2.0 +
+        Fun_Luminance(_Texture.Sample(_Sampler, In - _Off)) * 2.0 
     ) - _Alpha * 2.0;
 
     _Result.y = (
-        S2D_Image.Sample(S2D_ImageSampler, (In + _Size) ).a * 2.0 +
-        S2D_Image.Sample(S2D_ImageSampler, (In - _Size) ).a / 2.0 
+        Fun_Luminance(_Texture.Sample(_Sampler, In + _Off)) * 2.0 +
+        Fun_Luminance(_Texture.Sample(_Sampler, In - _Off)) / 2.0 
     ) - _Alpha;
 
     return _Result;
 }
 
-PS_OUTPUT ps_main( in PS_INPUT In )
+float4 Main(in PS_INPUT In, bool _Premultiplied) : SV_TARGET
 {
-    PS_OUTPUT Out;
+    float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint, _Premultiplied);
 
-    float4 _Render_Texture = S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint;
-    //float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.texCoord);
+        float4 _Result;
+        float2 _Render;
+        float2 _Offset = _Size * (-float2(fPixelWidth, fPixelHeight));
 
-    float4 _Result;
-    float2 _Result_Sharpness;
+        float _Sin;
+        float _Cos;
+        sincos(radians(_Angle), _Sin, _Cos);
+        _Offset = float2(
+            _Offset.x * _Cos - _Offset.y * _Sin,
+            _Offset.x * _Sin + _Offset.y * _Cos
+        );
 
-        //_Result_Sharpness = Fun_Border(In, 3.5, _Render_Texture.a);
-        _Result.rgb = float3(0.0, 0.5, 0.5);// - _Result_Sharpness.x * 0.3999;
+            _Result.rgb = _Color.rgb;
+            // float3(184.0, 219.0, 219.0)
         
-        /* ############################# */
-        _Result_Sharpness = Fun_Border(In.texCoord, 5., _Render_Texture.a);
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 * (_Result_Sharpness.y + 0.1), saturate((0.25 - saturate(abs(_Result_Sharpness.x * 6.0))) * 0.2));
+            /* ############################# */
+            
+            _Render = Fun_Border(S2D_Image, S2D_ImageSampler, In.texCoord, 5.0 * _Offset, _Render_Texture.a);
+            _Result.rgb = lerp(_Result.rgb, _ColorEx.rgb * (_Render.y + 0.1), saturate((0.25 - saturate(abs(_Render.x * 6.0))) * 0.2));
 
-        _Result_Sharpness = Fun_Border(In.texCoord, float2(-5., 5.), _Render_Texture.a);
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 * (_Result_Sharpness.y), saturate((0.25 - saturate(abs(_Result_Sharpness.x * 6.0))) * 0.2));
-        
-        /* ############################# */
+            _Render = Fun_Border(S2D_Image, S2D_ImageSampler, In.texCoord, float2(-5.0, 5.0), _Render_Texture.a);
+            _Result.rgb = lerp(_Result.rgb, _ColorEx.rgb * (_Render.y), saturate((0.25 - saturate(abs(_Render.x * 6.0))) * 0.2));
+            
+            /* ############################# */
 
-        /* ############################# */
-        _Result_Sharpness = Fun_Border(In.texCoord, 4.95, _Render_Texture.a);
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 * (_Result_Sharpness.y + 0.1), saturate((0.25 - saturate(abs(_Result_Sharpness.x * 6.0))) * 1.5));
+            /* ############################# */
+            
+            _Render = Fun_Border(S2D_Image, S2D_ImageSampler, In.texCoord, 4.95 * _Offset, _Render_Texture.a);
+            _Result.rgb = lerp(_Result.rgb, _ColorEx.rgb * (_Render.y + 0.1), saturate((0.25 - saturate(abs(_Render.x * 6.0))) * 1.5));
 
-        float2 _Result_Side_L = _Result_Sharpness;
-        _Result_Sharpness = Fun_Border(In.texCoord, float2(-4.95, 4.95), _Render_Texture.a);
-        float2 _Result_Side_R = _Result_Sharpness;
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 * (_Result_Sharpness.y), saturate((0.25 - saturate(abs(_Result_Sharpness.x * 6.0))) * 0.5));
-        
-        /* ############################# */
+            float2 _Result_Side_L = _Render;
+            _Render = Fun_Border(S2D_Image, S2D_ImageSampler, In.texCoord, float2(-4.95, 4.95) * _Offset, _Render_Texture.a);
+            _Result.rgb = lerp(_Result.rgb, _ColorEx.rgb * (_Render.y), saturate((0.25 - saturate(abs(_Render.x * 6.0))) * 0.5));
+            
+            /* ############################# */
 
-        /* ############################# */
-        _Result_Sharpness = Fun_Border(In.texCoord, 2.0, _Render_Texture.a) * 0.3;
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 + min(_Result_Sharpness.y - 0.09, 0.0), saturate(0.5 - saturate(abs(_Result_Sharpness.x * 6.0))));
+            /* ############################# */
+            
+            _Render = Fun_Border(S2D_Image, S2D_ImageSampler, In.texCoord, 2.0 * _Offset, _Render_Texture.a) * 0.3;
+            _Result.rgb = lerp(_Result.rgb, _ColorEx.rgb + min(_Render.y - 0.09, 0.0), saturate(0.5 - saturate(abs(_Render.x * 6.0))));
 
-        _Result_Sharpness = Fun_Border(In.texCoord, float2(-2.0, 2.0), _Render_Texture.a) * 0.3;
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 + min(_Result_Sharpness.y - 0.09, 0.0),  saturate(0.5 - saturate(abs(_Result_Sharpness.x * 6.0))));
-        /* ############################# */
+            _Render = Fun_Border(S2D_Image, S2D_ImageSampler, In.texCoord, float2(-2.0, 2.0), _Render_Texture.a) * 0.3;
+            _Result.rgb = lerp(_Result.rgb, _ColorEx.rgb + min(_Render.y - 0.09, 0.0),  saturate(0.5 - saturate(abs(_Render.x * 6.0))));
+            
+            /* ############################# */
 
-        /* ############################# */
-        _Result_Sharpness = Fun_Border(In.texCoord, 1.85, _Render_Texture.a) * 0.3;
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 + min(_Result_Sharpness.y - 0.09, 0.0) + _Result_Side_L.y * 0.1, saturate((1.0 - saturate(abs(_Result_Sharpness.x * 6.0))) - (_Result_Side_L.y * 0.4)));
+            /* ############################# */
 
-        _Result_Sharpness = Fun_Border(In.texCoord, float2(-1.85, 1.85), _Render_Texture.a) * 0.3;
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 + min(_Result_Sharpness.y - 0.09, 0.0), 1.0 - saturate(abs(_Result_Sharpness.x * 6.0)));
-        /* ############################# */
+            _Render = Fun_Border(S2D_Image, S2D_ImageSampler, In.texCoord, 1.85 * _Offset, _Render_Texture.a) * 0.3;
+            _Result.rgb = lerp(_Result.rgb, _ColorEx.rgb + min(_Render.y - 0.09, 0.0) + _Result_Side_L.y * 0.1, saturate((1.0 - saturate(abs(_Render.x * 6.0))) - (_Result_Side_L.y * 0.4)));
+
+            _Render = Fun_Border(S2D_Image, S2D_ImageSampler, In.texCoord, float2(-1.85, 1.85) * _Offset, _Render_Texture.a) * 0.3;
+            _Result.rgb = lerp(_Result.rgb, _ColorEx.rgb + min(_Render.y - 0.09, 0.0), 1.0 - saturate(abs(_Render.x * 6.0)));
+            
+            /* ############################# */
 
         _Result.rgb = lerp(_Render_Texture.rgb, _Result.rgb, _Mixing);
-        _Result.a =  _Render_Texture.a;
+        _Result.a = _Render_Texture.a;
 
-    Out.Color = _Result;
-
-    return Out;
+    return _Result;
 }
 
 /************************************************************/
-/* Premultiplied Alpha */
+/* Render */
 /************************************************************/
 
-float4 Demultiply(float4 _Color)
-{
-	if ( _Color.a != 0 )   _Color.rgb /= _Color.a;
-	return _Color;
+float4 ps_main(in PS_INPUT In) : SV_TARGET{
+    float4 _Render = Main(In, false);
+    return _Render;
 }
 
-PS_OUTPUT ps_main_pm( in PS_INPUT In ) 
+float4 ps_main_pm(in PS_INPUT In) : SV_TARGET
 {
-        PS_OUTPUT Out;
+    float4 _Render = Main(In, true);
+    _Render.rgb *= _Render.a;
 
-    float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord)) * In.Tint;
-    //float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.texCoord);
-
-    float4 _Result;
-    float2 _Result_Sharpness;
-
-        //_Result_Sharpness = Fun_Border(In, 3.5, _Render_Texture.a);
-        _Result.rgb = float3(0.0, 0.5, 0.5);// - _Result_Sharpness.x * 0.3999;
-        
-        /* ############################# */
-        _Result_Sharpness = Fun_Border(In.texCoord, 5., _Render_Texture.a);
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 * (_Result_Sharpness.y + 0.1), saturate((0.25 - saturate(abs(_Result_Sharpness.x * 6.0))) * 0.2));
-
-        _Result_Sharpness = Fun_Border(In.texCoord, float2(-5., 5.), _Render_Texture.a);
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 * (_Result_Sharpness.y), saturate((0.25 - saturate(abs(_Result_Sharpness.x * 6.0))) * 0.2));
-        
-        /* ############################# */
-
-        /* ############################# */
-        _Result_Sharpness = Fun_Border(In.texCoord, 4.95, _Render_Texture.a);
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 * (_Result_Sharpness.y + 0.1), saturate((0.25 - saturate(abs(_Result_Sharpness.x * 6.0))) * 1.5));
-
-        float2 _Result_Side_L = _Result_Sharpness;
-        _Result_Sharpness = Fun_Border(In.texCoord, float2(-4.95, 4.95), _Render_Texture.a);
-        float2 _Result_Side_R = _Result_Sharpness;
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 * (_Result_Sharpness.y), saturate((0.25 - saturate(abs(_Result_Sharpness.x * 6.0))) * 0.5));
-        
-        /* ############################# */
-
-        /* ############################# */
-        _Result_Sharpness = Fun_Border(In.texCoord, 2.0, _Render_Texture.a) * 0.3;
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 + min(_Result_Sharpness.y - 0.09, 0.0), saturate(0.5 - saturate(abs(_Result_Sharpness.x * 6.0))));
-
-        _Result_Sharpness = Fun_Border(In.texCoord, float2(-2.0, 2.0), _Render_Texture.a) * 0.3;
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 + min(_Result_Sharpness.y - 0.09, 0.0),  saturate(0.5 - saturate(abs(_Result_Sharpness.x * 6.0))));
-        /* ############################# */
-
-        /* ############################# */
-        _Result_Sharpness = Fun_Border(In.texCoord, 1.85, _Render_Texture.a) * 0.3;
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 + min(_Result_Sharpness.y - 0.09, 0.0) + _Result_Side_L.y * 0.1, saturate((1.0 - saturate(abs(_Result_Sharpness.x * 6.0))) - (_Result_Side_L.y * 0.4)));
-
-        _Result_Sharpness = Fun_Border(In.texCoord, float2(-1.85, 1.85), _Render_Texture.a) * 0.3;
-        _Result.rgb = lerp(_Result.rgb, float3(184.0, 219.0, 219.0) / 255.0 + min(_Result_Sharpness.y - 0.09, 0.0), 1.0 - saturate(abs(_Result_Sharpness.x * 6.0)));
-        /* ############################# */
-
-        _Result.rgb = lerp(_Render_Texture.rgb, _Result.rgb, _Mixing);
-        _Result.a =  _Render_Texture.a;
-    _Result.rgb *= _Result.a;
-
-    Out.Color = _Result;
-    return Out;
+    return _Render;
 }
