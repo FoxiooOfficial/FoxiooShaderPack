7ff45320f820f11d7496a32893e1dad876d9ea88
Foxioo <foxioo2137@gmail.com>
2026-08-27 19:15:57 +0200

    fxwg port for rainbow foilage

--- Foxioo Shader Pack/Special/Holographic/Foilage/Rainbow Foilage (Switch).hlsl
+++ Foxioo Shader Pack/Special/Holographic/Foilage/Rainbow Foilage (Switch).hlsl
@@ -1,10 +1,12 @@
 /***********************************************************/
 
-/* Shader author: Foxioo */
-/* Version shader: 1.1 (18.10.2025) */
-/* My GitHub: https://github.com/FoxiooOfficial */
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
 
 /***********************************************************/
+
 
 /* ####################################################### */
 
@@ -31,8 +33,6 @@ cbuffer PS_VARIABLES : register(b0)
     float _PosX;
     float _PosY;
     bool __;
-	bool _Is_Pre_296_Build;
-	bool ___;
 };
 
 struct PS_INPUT
@@ -69,13 +69,22 @@ float Fun_Luminance(float3 _Result)
     return _Y;
 }
 
-
-PS_OUTPUT ps_main( in PS_INPUT In )
+float4 Demultiply(float4 _Render, bool _Premultiplied)
 {
-    PS_OUTPUT Out;
+    if(_Premultiplied)
+    {
+	    if ( _Render.a != 0.0 ) {
+            _Render.rgb /= _Render.a;
+        }
+    }
 
-    float4 _Render_Texture = S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint;
-    float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord) * In.Tint;
+	return _Render;
+}
+
+float4 Main(in PS_INPUT In, bool _Premultiplied) : SV_TARGET
+{
+    float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint, _Premultiplied);
+    float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord);
 
         float4 _Render =    _Blending_Mode ? _Render_Background : _Render_Texture;
 
@@ -93,45 +102,22 @@ PS_OUTPUT ps_main( in PS_INPUT In )
         _Render.rgb = lerp(_Render.rgb, pow(abs(_Render.rgb * 0.45 + 0.75 * (_Lum * _Render.rgb * 0.5 + _Lum * 0.5)), 2.2), _Mixing);
     
     _Render.a = _Render_Texture.a;
-    Out.Color = _Render;
-    
-    return Out;
+    return _Render;
 }
 
 /************************************************************/
-/* Premultiplied Alpha */
+/* Render */
 /************************************************************/
 
-float4 Demultiply(float4 _Color)
-{
-	if ( _Color.a != 0 )   _Color.rgb /= _Color.a;
-	return _Color;
+float4 ps_main(in PS_INPUT In) : SV_TARGET{
+    float4 _Render = Main(In, false);
+    return _Render;
 }
 
-PS_OUTPUT ps_main_pm( in PS_INPUT In ) 
+float4 ps_main_pm(in PS_INPUT In) : SV_TARGET
 {
-    PS_OUTPUT Out;
-
-    float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord)) * In.Tint;
-    float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord) * In.Tint;
-
-        float4 _Render =    _Blending_Mode ? _Render_Background : _Render_Texture;
-
-            float3 _Lum = Fun_Luminance(_Render.rgb);
-            _Lum *= _Lum;
-
-                float2 _UV = In.texCoord + float2(_PosX, _PosY);
-
-                    _Lum.r = 0.5 + 0.5 * sin(_Phase * abs(cos(_UV.x + _Render.r + _Lum.r * 0.2 + 4.0))) * _Render.r;
-                    _Lum.g = 0.5 + 0.5 * sin(_Phase * abs(cos(_UV.x + _Render.g + _Lum.g * 0.2 + 2.0))) * _Render.g;
-                    _Lum.b = 0.5 + 0.5 * sin(_Phase * abs(cos(_UV.x + _Render.b + _Lum.b * 0.2 + 0.0))) * _Render.b;
-
-                //_Lum = abs(_Lum);
-
-        _Render.rgb = lerp(_Render.rgb, pow(abs(_Render.rgb * 0.45 + 0.75 * (_Lum * _Render.rgb * 0.5 + _Lum * 0.5)), 2.2), _Mixing);
-    _Render.a = _Render_Texture.a;
+    float4 _Render = Main(In, true);
     _Render.rgb *= _Render.a;
 
-    Out.Color = _Render;
-    return Out;
+    return _Render;
 }
