b376a91dab5d4f50a8d3b4213ae52be4641b5702
Foxioo <foxioo2137@gmail.com>
2026-08-28 00:27:27 +0200

    new shader: invert chroma

---
+++ Foxioo Shader Pack/Coloristic/Other/Inverted/Global/Invert Chroma (Switch).hlsl
@@ -0,0 +1,99 @@
+/***********************************************************/
+
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
+
+/***********************************************************/
+
+/* ####################################################### */
+
+/***********************************************************/
+/* Samplers */
+/***********************************************************/
+
+Texture2D<float4> S2D_Image : register(t0);
+SamplerState S2D_ImageSampler : register(s0);
+
+Texture2D<float4> S2D_Background : register(t1);
+SamplerState S2D_BackgroundSampler : register(s1);
+
+/***********************************************************/
+/* Variables */
+/***********************************************************/
+
+cbuffer PS_VARIABLES : register(b0)
+{
+    bool _;
+    bool _Blending_Mode;
+    float _Mixing;
+	bool __;
+};
+
+struct PS_INPUT
+{
+    float4 Tint : COLOR0;
+    float2 texCoord : TEXCOORD0;
+    float2 bgCoord : TEXCOORD1;
+};
+
+struct PS_OUTPUT
+{
+    float4 Color : SV_Target;
+};
+
+/************************************************************/
+/* Main */
+/************************************************************/
+
+float4 Demultiply(float4 _Render, bool _Premultiplied)
+{
+    if(_Premultiplied)
+    {
+	    if ( _Render.a != 0.0 ) {
+            _Render.rgb /= _Render.a;
+        }
+    }
+
+	return _Render;
+}
+
+float4 Main(in PS_INPUT In, bool _Premultiplied) : SV_TARGET
+{
+    float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint, _Premultiplied);
+    float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord);
+
+        float4 _Result;
+        _Result.rgb = lerp(_Render_Texture.rgb, _Render_Background.rgb, float(_Blending_Mode));
+        _Result.a = _Render_Texture.a;
+
+        float3 _Render = _Result.rgb;
+
+            float _Lum = dot(_Result.rgb, float3(0.299, 0.587, 0.114));
+            float _Delta = 0.5 - _Lum;
+
+            _Result.rgb += _Delta * 2.0;
+            _Result.rgb = 1.0 - _Result.rgb;
+
+        _Result.rgb = lerp(_Render, _Result.rgb, _Mixing);
+    
+    return _Result;
+}
+
+/************************************************************/
+/* Render */
+/************************************************************/
+
+float4 ps_main(in PS_INPUT In) : SV_TARGET{
+    float4 _Render = Main(In, false);
+    return _Render;
+}
+
+float4 ps_main_pm(in PS_INPUT In) : SV_TARGET
+{
+    float4 _Render = Main(In, true);
+    _Render.rgb *= _Render.a;
+
+    return _Render;
+}
