eae6fb2e03dd5d1a84c20b60ea6e05bda538167d
Foxioo <foxioo2137@gmail.com>
2026-08-27 17:54:06 +0200

    fxwg port for cpu foilage

--- Foxioo Shader Pack/Special/Holographic/Foilage/CPU Foilage (Switch).fx
+++ Foxioo Shader Pack/Special/Holographic/Foilage/CPU Foilage (Switch).fx
@@ -1,8 +1,9 @@
 /***********************************************************/
 
-/* Shader author: Foxioo */
-/* Version shader: 1.2 (18.10.2025) */
-/* My GitHub: https://github.com/FoxiooOfficial */
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
 
 /***********************************************************/
 
@@ -33,14 +34,30 @@ sampler2D S2D_Background : register(s1);
 /* Main */
 /***********************************************************/
 
+#define POINTS float2(_PointX, _PointY)
+#define RAD 0.01745329251
+
 float2 Fun_RotationX(float2 In)
 {
-    float2  _UV = float2((In.x + _PointX) / 2.0, (In.y + _PointY) / 2.0);
-    _RotX = _RotX * (3.14159265 / 180);
+    float2 UV;
+    float _Sin, _Cos;
+    sincos(RAD * _RotX, _Sin, _Cos);
 
-        _UV = mul(float2x2(cos(_RotX), sin(_RotX), -sin(_RotX), cos(_RotX)), _UV);
+        UV = POINTS + mul(float2x2(_Cos, _Sin, -_Sin, _Cos), In - POINTS);
 
-    return _UV;
+    return UV;
+}
+
+float Fun_Channel(float2 UV, int _Mode)
+{
+    if(_Mode == 0)
+        return UV.x;
+    
+    else if(_Mode == 1)
+        return UV.y;
+
+    else
+        return UV.x + UV.y;
 }
 
 float3 Fun_Rainbow(float2 In)
@@ -48,10 +65,9 @@ float3 Fun_Rainbow(float2 In)
     static const float _Frag = 6.28318;
     float3 _Render;
     
-    float _Red, _Green, _Blue;
-    if(_ChannelRed == 0) _Red = In.x;       else if (_ChannelRed == 1) _Red = In.y;         else  _Red = In.x + In.y;
-    if(_ChannelGreen == 0) _Green = In.x;   else if (_ChannelGreen == 1) _Green = In.y;     else  _Green = In.x + In.y;
-    if(_ChannelBlue == 0) _Blue = In.x;     else if (_ChannelBlue == 1) _Blue = In.y;       else  _Blue = In.x + In.y;
+    float _Red = Fun_Channel(In, _ChannelRed);
+    float _Green = Fun_Channel(In, _ChannelGreen);
+    float _Blue = Fun_Channel(In, _ChannelBlue);
     
     _Render.r = sin(_Frag * _Red + 0.0) * 0.5 + 0.5;
     _Render.g = sin(_Frag * _Green + 2.0) * 0.5 + 0.5;
@@ -65,17 +81,7 @@ float4 ps_main(in float2 In : TEXCOORD0, in float2 In_Background : TEXCOORD1) : 
     float4 _Render_Texture = tex2D(S2D_Image, In);
     float4 _Render_Background = tex2D(S2D_Background, In_Background);
 
-    float4 _Result;
-
-    if(_Blending_Mode == 0)
-    {
-        _Result = _Render_Texture;
-    }
-    else
-    {
-        _Result = _Render_Background;
-    }
-
+    float4 _Result = _Blending_Mode ? _Render_Background : _Render_Texture;
     _Result.a = _Render_Texture.a;
 
         float _Average = (_Result.r + _Result.g + _Result.b) / 3.0;
@@ -83,7 +89,6 @@ float4 ps_main(in float2 In : TEXCOORD0, in float2 In_Background : TEXCOORD1) : 
         float2  _UV = Fun_RotationX(In * _Average),
                 _ScaleTemp = (float2(_ScaleX, _ScaleY)) * _Scale,
                 _Pos = float2(-_PosX, _PosY);
-
 
         float3 _Render_Rainbow = Fun_Rainbow((_UV - _Pos) * _ScaleTemp);
 
--- Foxioo Shader Pack/Special/Holographic/Foilage/CPU Foilage (Switch).hlsl
+++ Foxioo Shader Pack/Special/Holographic/Foilage/CPU Foilage (Switch).hlsl
@@ -1,8 +1,9 @@
 /***********************************************************/
 
-/* Shader author: Foxioo */
-/* Version shader: 1.2 (18.10.2025) */
-/* My GitHub: https://github.com/FoxiooOfficial */
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
 
 /***********************************************************/
 
@@ -29,33 +30,30 @@ cbuffer PS_VARIABLES : register(b0)
     float _PosX;
     float _PosY;
     bool ___;
-    float _RotX;
-    bool ____;
+    float _RotX; 
     float _PointX;
     float _PointY;
-    bool _____;
+    bool ____;
     float _Scale;
     float _ScaleX;
     float _ScaleY;
-    bool ______;
+    bool _____;
     bool _Blending_Mode;
     float _Mixing;
     int _ChannelRed;
-    int _ChannelGreen;
+    int _ChannelGreen; 
     int _ChannelBlue;
-    bool _______;
-
-	bool _Is_Pre_296_Build;
-	bool ________;
 };
+
+float _Mul;
 
 struct PS_INPUT
 {
-  float4 Tint : COLOR0;
-  float2 texCoord : TEXCOORD0;
-  float2 bgCoord : TEXCOORD1;
+    float4 Tint : COLOR0;
+    float2 texCoord : TEXCOORD0;
+	float2 bgCoord : TEXCOORD1;
+    float4 Position : SV_POSITION;
 };
-
 
 struct PS_OUTPUT
 {
@@ -72,25 +70,40 @@ cbuffer PS_PIXELSIZE : register(b1)
 /* Main */
 /************************************************************/
 
+#define POINTS float2(_PointX, _PointY)
+#define RAD 0.01745329251
+
 float2 Fun_RotationX(float2 In)
 {
-    float2  _UV = float2((In.x + _PointX) / 2.0, (In.y + _PointY) / 2.0);
-    float _RotXTemp = _RotX * (3.14159265 / 180);
+    float2 UV;
+    float _Sin, _Cos;
+    sincos(RAD * _RotX, _Sin, _Cos);
 
-        _UV = mul(float2x2(cos(_RotXTemp), sin(_RotXTemp), -sin(_RotXTemp), cos(_RotXTemp)), _UV);
+        UV = POINTS + mul(float2x2(_Cos, _Sin, -_Sin, _Cos), In - POINTS);
 
-    return _UV;
+    return UV;
 }
 
-float3 Fun_Rainbow(float2 In)
+float Fun_Channel(float2 UV, int _Mode)
+{
+    if(_Mode == 0)
+        return UV.x;
+    
+    else if(_Mode == 1)
+        return UV.y;
+
+    else
+        return UV.x + UV.y;
+}
+
+float3 Fun_Rainbow(float2 UV)
 {
     static const float _Frag = 6.28318;
     float3 _Render;
     
-    float _Red, _Green, _Blue;
-    if(_ChannelRed == 0) _Red = In.x;       else if (_ChannelRed == 1) _Red = In.y;         else  _Red = In.x + In.y;
-    if(_ChannelGreen == 0) _Green = In.x;   else if (_ChannelGreen == 1) _Green = In.y;     else  _Green = In.x + In.y;
-    if(_ChannelBlue == 0) _Blue = In.x;     else if (_ChannelBlue == 1) _Blue = In.y;       else  _Blue = In.x + In.y;
+    float _Red = Fun_Channel(UV, _ChannelRed);
+    float _Green = Fun_Channel(UV, _ChannelGreen);
+    float _Blue = Fun_Channel(UV, _ChannelBlue);
     
     _Render.r = sin(_Frag * _Red + 0.0) * 0.5 + 0.5;
     _Render.g = sin(_Frag * _Green + 2.0) * 0.5 + 0.5;
@@ -99,87 +112,52 @@ float3 Fun_Rainbow(float2 In)
     return _Render;
 }
 
-
-PS_OUTPUT ps_main( in PS_INPUT In )
+float4 Demultiply(float4 _Render, bool _Premultiplied)
 {
-    PS_OUTPUT Out;
-
-    float4 _Render_Texture = S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint;
-    float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord);
-
-    float4 _Result;
-
-        if(_Blending_Mode == 0)
-        {
-            _Result = _Render_Texture;
+    if(_Premultiplied)
+    {
+	    if ( _Render.a != 0.0 ) {
+            _Render.rgb /= _Render.a;
         }
-        else
-        {
-            _Result = _Render_Background;
-        }
+    }
 
-        _Result.a = _Render_Texture.a;
-
-            float _Average = (_Result.r + _Result.g + _Result.b) / 3.0;
-
-            float2  _UV = Fun_RotationX(In.texCoord * _Average),
-                    _ScaleTemp = (float2(_ScaleX, _ScaleY)) * _Scale,
-                    _Pos = float2(-_PosX, _PosY);
-
-
-            float3 _Render_Rainbow = Fun_Rainbow((_UV - _Pos) * _ScaleTemp);
-
-        _Result.rgb += _Render_Rainbow.rgb * _Average * _Mixing;
-
-    _Result.a = _Render_Texture.a;
-    Out.Color = _Result;
-    
-    return Out;
-}
-/************************************************************/
-/* Premultiplied Alpha */
-/************************************************************/
-
-float4 Demultiply(float4 _Color)
-{
-	if ( _Color.a != 0 )   _Color.rgb /= _Color.a;
-	return _Color;
+	return _Render;
 }
 
-PS_OUTPUT ps_main_pm( in PS_INPUT In ) 
+float4 Main(in PS_INPUT In, bool _Premultiplied) : SV_TARGET
 {
-    PS_OUTPUT Out;
-
-    float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord)) * In.Tint;
+    float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint, _Premultiplied);
     float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord);
 
-    float4 _Result;
-
-        if(_Blending_Mode == 0)
-        {
-            _Result = _Render_Texture;
-        }
-        else
-        {
-            _Result = _Render_Background;
-        }
-
-        _Result.a = _Render_Texture.a;
-
-            float _Average = (_Result.r + _Result.g + _Result.b) / 3.0;
-
-            float2  _UV = Fun_RotationX(In.texCoord * _Average),
-                    _ScaleTemp = (float2(_ScaleX, _ScaleY)) * _Scale,
-                    _Pos = float2(-_PosX, _PosY);
-
-
-            float3 _Render_Rainbow = Fun_Rainbow((_UV - _Pos) * _ScaleTemp);
-
-        _Result.rgb += _Render_Rainbow.rgb * _Average * _Mixing;
-
+    float4 _Result = _Blending_Mode ? _Render_Background : _Render_Texture;
     _Result.a = _Render_Texture.a;
-    _Result.rgb *= _Result.a;
 
-    Out.Color = _Result;
-    return Out;  
+        float _Average = (_Result.r + _Result.g + _Result.b) / 3.0;
+
+        float2  _UV = Fun_RotationX(In.texCoord * _Average),
+                _ScaleTemp = (float2(_ScaleX, _ScaleY)) * _Scale,
+                _Pos = float2(-_PosX, _PosY);
+
+        float3 _Render_Rainbow = Fun_Rainbow((_UV - _Pos) * _ScaleTemp);
+
+    _Result.rgb += _Render_Rainbow.rgb * _Average * _Mixing;
+
+    return _Result;
+}
+
+/************************************************************/
+/* Render */
+/************************************************************/
+
+float4 ps_main(in PS_INPUT In) : SV_TARGET{
+    float4 _Render = Main(In, false);
+    return _Render;
+}
+
+float4 ps_main_pm(in PS_INPUT In) : SV_TARGET
+{
+    float4 _Render = Main(In, true);
+    _Render.rgb *= _Render.a;
+
+    return _Render;
 }
