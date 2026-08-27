1482a8e7d82b24c802a740832d7c4195d36ebdb5
Foxioo <foxioo2137@gmail.com>
2026-08-27 16:06:39 +0200

    fxwg port for flip switch + rework

--- Foxioo Shader Pack/Transformations/Geometric Transformations/Affine Transformations/Flip (Switch).fx
+++ Foxioo Shader Pack/Transformations/Geometric Transformations/Affine Transformations/Flip (Switch).fx
@@ -1,8 +1,9 @@
 /***********************************************************/
 
-/* Shader author: Foxioo */
-/* Version shader: 1.1 (18.10.2025) */
-/* My GitHub: https://github.com/FoxiooOfficial */
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
 
 /***********************************************************/
 
@@ -19,12 +20,6 @@ sampler2D S2D_Background : register(s1);
 /* Variables */
 /***********************************************************/
 
-    float   _PosX, _PosY,
-            _RotX,
-            _PointX, _PointY,
-            _ScaleX, _ScaleY, _Scale,
-            _Mixing, _Looping_Mode;
-
     bool    _Blending_Mode,
             _X, _Y;
 
@@ -32,62 +27,28 @@ sampler2D S2D_Background : register(s1);
 /* Main */
 /************************************************************/
 
-float2 Fun_RotationX(float2 In)
-{
-    float2 _Points = float2(_PointX, _PointY);
-    float2 _UV = In;
-    float _RotX_Fix = _RotX * (3.14159265 / 180);
-
-        _UV = _Points + mul(float2x2(cos(_RotX_Fix), sin(_RotX_Fix), -sin(_RotX_Fix), cos(_RotX_Fix)), _UV - _Points);
-
-    return _UV;
-}
-
 float4 ps_main(in float2 In : TEXCOORD0, in float2 In_Background : TEXCOORD1) : COLOR0
 {   
-    float4 _Render_Texture = tex2D(S2D_Image, In);
-    float4 _Render_Background = tex2D(S2D_Background, In_Background);
     float4 _Render;
 
-    float2  _Pos = float2(_PosX, _PosY),
-        UV = Fun_RotationX((In + _Pos));
-        UV = ((UV - float2(_PointX, _PointY)) * float2(_ScaleX, _ScaleY) * _Scale) + float2(_PointX, _PointY);
-
-        float2 _UV_Temp = UV;
-        if(_X) _UV_Temp.x = 1.0 - UV.x;
-        if(_Y) _UV_Temp.y = 1.0 - UV.y;
-
-        UV = lerp(UV, _UV_Temp, _Mixing);
-
-
-        if (_Looping_Mode == 0) {
-            UV = frac(UV);
-        }
-            else if(_Looping_Mode == 1)
-            {
-                UV /= 2;
-                UV = frac(UV);
-                UV = abs(UV * 2.0 - 1.0);
-            }
-            else if(_Looping_Mode == 2)
-            {
-                UV = clamp(UV, 0.0, 1.0);
-            }
-
-    if(_Blending_Mode == 0) {   _Render = tex2D(S2D_Image, UV);  }
-    else {                      _Render = float4( tex2D(S2D_Background, UV).rgb, _Render_Texture.a);  }
-
-        if (_Looping_Mode == 3 && any(UV < 0.0 || UV > 1.0))
+        if(_Blending_Mode)
         {
-            _Render = 0;
-        }
+            float2 UV = abs(float2(_X, _Y) - In_Background);
 
+            _Render.rgb = tex2D(S2D_Background, UV).rgb;
+            _Render.a = tex2D(S2D_Image, In).a;
+        }
+        else
+        {
+            float2 UV = abs(float2(_X, _Y) - In);
+            _Render = tex2D(S2D_Image, UV);
+        }
+        
     return _Render;
-    
 }
 
 /************************************************************/
 /* Tech Main */
 /************************************************************/
 
-technique tech_main { pass P0 { PixelShader = compile ps_2_0 ps_main(); } }
+technique tech_main { pass P0 { PixelShader = compile ps_1_4 ps_main(); } }
--- Foxioo Shader Pack/Transformations/Geometric Transformations/Affine Transformations/Flip (Switch).hlsl
+++ Foxioo Shader Pack/Transformations/Geometric Transformations/Affine Transformations/Flip (Switch).hlsl
@@ -1,8 +1,9 @@
 /***********************************************************/
 
-/* Shader author: Foxioo */
-/* Version shader: 1.1 (18.10.2025) */
-/* My GitHub: https://github.com/FoxiooOfficial */
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
 
 /***********************************************************/
 
@@ -11,7 +12,6 @@
 /***********************************************************/
 /* Samplers */
 /***********************************************************/
-
 
 Texture2D<float4> S2D_Image : register(t0);
 SamplerState S2D_ImageSampler : register(s0);
@@ -23,31 +23,13 @@ SamplerState S2D_BackgroundSampler : register(s1);
 /* Variables */
 /***********************************************************/
 
-
 cbuffer PS_VARIABLES : register(b0)
 {
     bool _;
-    bool __;
-    float _PosX;
-    float _PosY;
-    bool ___;
-    float _RotX;
-    bool ____;
-    float _PointX;
-    float _PointY;
-    bool _____;        
-    float _Scale;
-    float _ScaleX;
-    float _ScaleY;
-    bool ______;
-    int _Looping_Mode;
     bool _Blending_Mode;
-    float _Mixing;
     bool _X;
     bool _Y;
-    bool _______;
-	bool _Is_Pre_296_Build;
-	bool ________;
+    bool __;
 };
 
 struct PS_INPUT
@@ -73,115 +55,51 @@ cbuffer PS_PIXELSIZE : register(b1)
 /* Main */
 /************************************************************/
 
-float2 Fun_RotationX(float2 In)
+float4 Demultiply(float4 _Render, bool _Premultiplied)
 {
-    float2 _Points = float2(_PointX, _PointY);
-    float2 _UV = In;
-    float _RotX_Fix = _RotX * (3.14159265 / 180);
+    if(_Premultiplied)
+    {
+	    if ( _Render.a != 0.0 ) {
+            _Render.rgb /= _Render.a;
+        }
+    }
 
-        _UV = _Points + mul(float2x2(cos(_RotX_Fix), sin(_RotX_Fix), -sin(_RotX_Fix), cos(_RotX_Fix)), _UV - _Points);
-
-    return _UV;
+	return _Render;
 }
 
-PS_OUTPUT ps_main( in PS_INPUT In )
-{ 
-    PS_OUTPUT Out;
+float4 Main(in PS_INPUT In, bool _Premultiplied) : SV_TARGET
+{
+    float4 _Render;
 
-    float4 _Render_Texture = S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint;
-    float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord);
-    float4 _Render = 0;
-
-    float2  _Pos = float2(_PosX, _PosY),
-        UV = Fun_RotationX((In.texCoord + _Pos));
-        UV = ((UV - float2(_PointX, _PointY)) * float2(_ScaleX, _ScaleY) * _Scale) + float2(_PointX, _PointY);
-
-        float2 _UV_Temp = UV;
-        if(_X) _UV_Temp.x = 1.0 - UV.x;
-        if(_Y) _UV_Temp.y = 1.0 - UV.y;
-
-        UV = lerp(UV, _UV_Temp, _Mixing);
-
-            if(_Looping_Mode == 0)
-            {
-                UV = frac(UV);
-            }
-            else if(_Looping_Mode == 1)
-            {
-                UV /= 2;
-                UV = frac(UV);
-                UV = abs(UV * 2.0 - 1.0);
-            }
-            else if(_Looping_Mode == 2)
-            {
-                UV = clamp(UV, 0.0, 1.0);
-            }
-
-    if(_Blending_Mode == 0) {   _Render = S2D_Image.Sample(S2D_ImageSampler, UV) * In.Tint; }
-    else {                      _Render = float4(S2D_Background.Sample(S2D_BackgroundSampler, UV).rgb, _Render_Texture.a);    }
-
-        if (_Looping_Mode == 3 && any(UV < 0.0 || UV > 1.0))
+        if(_Blending_Mode)
         {
-            _Render = 0;
-        }
+            float2 UV = abs(float2(_X, _Y) - In.bgCoord);
 
-    Out.Color = _Render;
-    return Out;
+            _Render.rgb = S2D_Background.Sample(S2D_BackgroundSampler, UV).rgb;
+            _Render.a = S2D_Image.Sample(S2D_ImageSampler, In.texCoord).a * In.Tint.a;
+        }
+        else
+        {
+            float2 UV = abs(float2(_X, _Y) - In.texCoord);
+            _Render = Demultiply(S2D_Image.Sample(S2D_ImageSampler, UV) * In.Tint, _Premultiplied);
+        }
+        
+    return _Render;
 }
 
 /************************************************************/
-/* Premultiplied Alpha */
+/* Render */
 /************************************************************/
 
-float4 Demultiply(float4 _Color)
-{
-	if ( _Color.a != 0 )   _Color.rgb /= _Color.a;
-	return _Color;
+float4 ps_main(in PS_INPUT In) : SV_TARGET{
+    float4 _Render = Main(In, false);
+    return _Render;
 }
 
-PS_OUTPUT ps_main_pm( in PS_INPUT In )
-{ 
-    PS_OUTPUT Out;
-
-    float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint);
-    float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord);
-    float4 _Render = 0;
-
-    float2  _Pos = float2(_PosX, _PosY),
-        UV = Fun_RotationX((In.texCoord + _Pos));
-        UV = ((UV - float2(_PointX, _PointY)) * float2(_ScaleX, _ScaleY) * _Scale) + float2(_PointX, _PointY);
-
-        float2 _UV_Temp = UV;
-        if(_X) _UV_Temp.x = 1.0 - UV.x;
-        if(_Y) _UV_Temp.y = 1.0 - UV.y;
-
-        UV = lerp(UV, _UV_Temp, _Mixing);
-
-            if(_Looping_Mode == 0)
-            {
-                UV = frac(UV);
-            }
-            else if(_Looping_Mode == 1)
-            {
-                UV /= 2;
-                UV = frac(UV);
-                UV = abs(UV * 2.0 - 1.0);
-            }
-            else if(_Looping_Mode == 2)
-            {
-                UV = clamp(UV, 0.0, 1.0);
-            }
-
-    if(_Blending_Mode == 0) {   _Render = Demultiply(S2D_Image.Sample(S2D_ImageSampler, UV)) * In.Tint; }
-    else {                      _Render = float4(S2D_Background.Sample(S2D_BackgroundSampler, UV).rgb, _Render_Texture.a);    }
-
-        if (_Looping_Mode == 3 && any(UV < 0.0 || UV > 1.0))
-        {
-            _Render = 0;
-        }
-
+float4 ps_main_pm(in PS_INPUT In) : SV_TARGET
+{
+    float4 _Render = Main(In, true);
     _Render.rgb *= _Render.a;
 
-    Out.Color = _Render;
-    return Out;
+    return _Render;
 }
