6b2a2f33211ac4355daf7fdfb8e0c04a43c223bb
Foxioo <foxioo2137@gmail.com>
2026-08-27 16:52:35 +0200

    fxwg port for cleartype

--- Foxioo Shader Pack/Special/Blur/Antialiasing/ClearType (Switch).hlsl
+++ Foxioo Shader Pack/Special/Blur/Antialiasing/ClearType (Switch).hlsl
@@ -1,8 +1,9 @@
 /***********************************************************/
 
-/* Shader author: Foxioo */
-/* Version shader: 1.5 (18.10.2025) */
-/* My GitHub: https://github.com/FoxiooOfficial */
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
 
 /***********************************************************/
 
@@ -28,8 +29,6 @@ cbuffer PS_VARIABLES : register(b0)
     bool _Blending_Mode;
     float _Mixing;
     bool __;
-	bool _Is_Pre_296_Build;
-	bool ___;
 };
 
 struct PS_INPUT
@@ -133,41 +132,41 @@ float4 Fun_AA(Texture2D _Texture2D, SamplerState _SamplerState, float2 In, float
     return lerp(_Render, _ClearType, _Mixing) * _Alpha;
 }
 
-PS_OUTPUT ps_main( in PS_INPUT In )
+float4 Demultiply(float4 _Render, bool _Premultiplied)
 {
-    PS_OUTPUT Out;
+    if(_Premultiplied)
+    {
+	    if ( _Render.a != 0.0 ) {
+            _Render.rgb /= _Render.a;
+        }
+    }
 
-    float4 _Render_Texture = S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint;
+	return _Render;
+}
+
+float4 Main(in PS_INPUT In, bool _Premultiplied) : SV_TARGET
+{
+    float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint, _Premultiplied);
     float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord);
 
-        float4 _Render = _Blending_Mode ? Fun_AA(S2D_Background, S2D_BackgroundSampler, In.bgCoord, _Render_Background, _Render_Texture.a) : Fun_AA(S2D_Image, S2D_ImageSampler, In.texCoord, _Render_Texture, 1.0) * In.Tint;
+        float4 _Result = _Blending_Mode ? Fun_AA(S2D_Background, S2D_BackgroundSampler, In.bgCoord, _Render_Background, _Render_Texture.a) : Fun_AA(S2D_Image, S2D_ImageSampler, In.texCoord, _Render_Texture, 1.0) * In.Tint;
 
-    Out.Color = _Render;
-    
-    return Out;
+    return _Result;
 }
 
 /************************************************************/
-/* Premultiplied Alpha */
+/* Render */
 /************************************************************/
 
-float4 Demultiply(float4 _Color)
-{
-	if ( _Color.a != 0 )   _Color.rgb /= _Color.a;
-	return _Color;
+float4 ps_main(in PS_INPUT In) : SV_TARGET{
+    float4 _Render = Main(In, false);
+    return _Render;
 }
 
-PS_OUTPUT ps_main_pm( in PS_INPUT In ) 
+float4 ps_main_pm(in PS_INPUT In) : SV_TARGET
 {
-    PS_OUTPUT Out;
-
-    float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint);
-    float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord);
-
-        float4 _Render = _Blending_Mode ? Fun_AA(S2D_Background, S2D_BackgroundSampler, In.bgCoord, _Render_Background, _Render_Texture.a) : Demultiply(Fun_AA(S2D_Image, S2D_ImageSampler, In.texCoord, _Render_Texture, 1.0) * In.Tint);
-
+    float4 _Render = Main(In, true);
     _Render.rgb *= _Render.a;
 
-    Out.Color = _Render;
-    return Out;
+    return _Render;
 }
