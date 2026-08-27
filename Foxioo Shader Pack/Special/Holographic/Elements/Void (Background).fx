5bfa7492f859ea4a8fe8ea74f345b7539185c3a6
Foxioo <foxioo2137@gmail.com>
2026-08-27 23:52:32 +0200

    fxwg port for void + rework

--- Foxioo Shader Pack/Special/Holographic/Elements/Void (Background).fx
+++ Foxioo Shader Pack/Special/Holographic/Elements/Void (Background).fx
@@ -1,8 +1,9 @@
 /***********************************************************/
 
-/* Shader author: Foxioo */
-/* Version shader: 1.1 (18.10.2025) */
-/* My GitHub: https://github.com/FoxiooOfficial */
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
 
 /***********************************************************/
 
@@ -32,77 +33,57 @@ sampler2D S2D_Background : register(s1);
 /* Main */
 /************************************************************/
 
-float Fun_Lum (float4 _Result) { return (0.2126 * _Result.r + 0.7152 * _Result.g + 0.0722 * _Result.b) * _Result.a; }
+static const int SAMPLES_INNER  = 12;
+#define PIXELSIZE               float2(fPixelWidth, fPixelHeight)
 
-float Fun_OuterOutline(float2 In)
+float Fun_Inner(float2 In)
 {
-    float2 _PX = float2(fPixelWidth, fPixelHeight);
-    float _Alpha = tex2D(S2D_Image, In).a;
+    float _Alpha = 0.0;
+    for(int y = 0; y <= SAMPLES_INNER; y++)
+    {
+        for(int x = 0; x <= SAMPLES_INNER; x++)
+        {
+            float2 _Offset = (float2(x, y) / (float)SAMPLES_INNER - 0.5);
+            float _Render = tex2D(S2D_Image, In + _Offset * _Border * PIXELSIZE).a;
 
-    float aL = tex2D(S2D_Image, In + float2(-_PX.x * _Border, 0.0)).a;
-    float aR = tex2D(S2D_Image, In + float2( _PX.x * _Border, 0.0)).a;
-    float aU = tex2D(S2D_Image, In + float2(0.0, -_PX.y * _Border)).a;
-    float aD = tex2D(S2D_Image, In + float2(0.0,  _PX.y * _Border)).a;
-
-
-    return step(0.01, aL + aR + aU + aD) * (1.0 - _Alpha) * ( (aL + aR + aU + aD));
+            _Alpha += _Render;
+        }
+    }
+    return _Alpha / float(SAMPLES_INNER * SAMPLES_INNER);
 }
 
-float3 Fun_Outline(float2 In, float3 _Color)
-{
-    float2 _PX = float2(fPixelWidth, fPixelHeight);
-    float _Alpha = tex2D(S2D_Image, In).a;
-
-    float aL1 = tex2D(S2D_Image, In + float2(-_PX.x * _Border, 0.0)).a;
-    float aR1 = tex2D(S2D_Image, In + float2( _PX.x * _Border, 0.0)).a;
-    float aU1 = tex2D(S2D_Image, In + float2(0.0, -_PX.y * _Border)).a;
-    float aD1 = tex2D(S2D_Image, In + float2(0.0,  _PX.y * _Border)).a;
-
-    float aL2 = tex2D(S2D_Image, In + float2(-_PX.x * 2.0 * _Border, 0.0)).a;
-    float aR2 = tex2D(S2D_Image, In + float2( _PX.x * 2.0 * _Border, 0.0)).a;
-    float aU2 = tex2D(S2D_Image, In + float2(0.0, -_PX.y * 2.0 * _Border)).a;
-    float aD2 = tex2D(S2D_Image, In + float2(0.0,  _PX.y * 2.0 * _Border)).a;
-
-        float _Edge1  = step(0.01, abs(aL1 - _Alpha) + abs(aR1 - _Alpha) + abs(aU1 - _Alpha) + abs(aD1 - _Alpha));
-        float _Edge2 = step(0.01, abs(aL2 - _Alpha) + abs(aR2 - _Alpha) + abs(aU2 - _Alpha) + abs(aD2 - _Alpha));
-
-    return lerp(_Color * 0.5, float3(1.0, 1.0, 1.0), _Edge1 * _Alpha) + _Edge2 * 0.25;
+float Fun_Lum (float4 _Result) { 
+    return (0.2126 * _Result.r + 0.7152 * _Result.g + 0.0722 * _Result.b) * _Result.a;
 }
 
-float Fun_Random(float2 In)
-{
+float Fun_Random(float2 In) {
     return frac(sin(dot(In, float2(12.9898, 78.233))) * 43758.5453);
 }
 
 float4 ps_main(in float2 In : TEXCOORD0, in float2 In_Background : TEXCOORD1) : COLOR0
 {
-    In = frac(In);
-
     float4 _Render_Texture = tex2D(S2D_Image, In);
     float4 _Render_Background = tex2D(S2D_Background, In_Background);
         
-        float _Rand = Fun_Random(In + _Render_Texture.rb + _Render_Texture.bg + _Render_Background.rg + _Render_Background.br);
-
-        float _Lum = Fun_Lum(_Render_Texture);
-        float _Lum_Background = Fun_Lum(_Render_Background);
+    float _Rand = Fun_Random(In + _Render_Texture.rb + _Render_Texture.bg + _Render_Background.rg + _Render_Background.br);
+    float _Lum = Fun_Lum(_Render_Texture);
+    float _Lum_Background = Fun_Lum(_Render_Background);
 
         float3 _Space = lerp(float3(0.0, 0.0, 0.0), float3(0.15, 0.05, 0.25), _Lum);
         _Space += (_Space * (tex2D(S2D_Background, In_Background + tan(_Lum) * _Lum * 0.25 - tan(_Lum_Background) * 0.25 - _Rand).rgb) * 1.5);
         _Space *= 0.5;
 
-            float _Render_Background_Void_R = tex2D(S2D_Background, In_Background + (_Render_Background.r * (1.0 - _Render_Texture.r) + _Rand) * 0.1).r;
-            float _Render_Background_Void_G = tex2D(S2D_Background, In_Background + (_Render_Background.g * (1.0 - _Render_Texture.g) + _Rand) * 0.1).b;
-            float _Render_Background_Void_B = tex2D(S2D_Background, In_Background + (_Render_Background.b * (1.0 - _Render_Texture.b) + _Rand) * 0.1).b;
+            float3 _Void = tex2D(S2D_Background, In_Background + (_Render_Background * (1.0 - _Render_Texture) + _Rand) * 0.1).rgb;
+            _Space = lerp(_Space, _Space + _Void * float3(0.15, 0.05, 0.25), 1.0 - _Lum);
 
-            _Space = lerp(_Space, _Space + float3(_Render_Background_Void_R, _Render_Background_Void_G, _Render_Background_Void_B) * float3(0.15, 0.05, 0.25), 1.0 - _Lum);
+            float4 _Result = _Render_Texture;
+            _Result.rgb = lerp( _Render_Texture.rgb, 
+                                _Space + pow(Fun_Random(In + _Space.rb + _Space.bg + _Render_Background.rg + _Render_Background.br + _Rand), 255.0), 
+                                _Mixing);
 
-
-        float4 _Result = _Render_Texture;
-        _Result.rgb = lerp(_Render_Texture.rgb, _Space + pow(Fun_Random(In + _Space.rb + _Space.bg + _Render_Background.rg + _Render_Background.br + _Rand), 255.0), _Mixing);
-        _Result.rgb += (Fun_Outline(In, _Space) + pow(_Lum, 6.0)) * saturate(_Mixing);
-
-        float3 _Cosmos = pow(Fun_Random(In), 128.0) * 0.5;
-        _Result = lerp(float4(_Cosmos.rgb, 1.0), _Result, 1.0 - pow(Fun_OuterOutline(In), 4.0));
+            float _Inner = (1.0 - Fun_Inner(In)) * _Render_Texture.a;
+            _Result.rgb += ((_Inner / pow(cos(_Lum - _Inner), 6.0)) * saturate(_Mixing)) * 0.05;
+            _Result.rgb += saturate(_Inner * _Inner + (1.0 - _Lum) * 0.15) * 4.0;
 
         _Result = lerp(_Render_Texture, _Result, _Mixing);
 
@@ -113,4 +94,4 @@ float4 ps_main(in float2 In : TEXCOORD0, in float2 In_Background : TEXCOORD1) : 
 /* Tech Main */
 /************************************************************/
 
-technique tech_main { pass P0 { PixelShader = compile ps_2_a ps_main(); } }
+technique tech_main { pass P0 { PixelShader = compile ps_3_0 ps_main(); } }
--- Foxioo Shader Pack/Special/Holographic/Elements/Void (Background).hlsl
+++ Foxioo Shader Pack/Special/Holographic/Elements/Void (Background).hlsl
@@ -1,8 +1,9 @@
 /***********************************************************/
 
-/* Shader author: Foxioo */
-/* Version shader: 1.1 (18.10.2025) */
-/* My GitHub: https://github.com/FoxiooOfficial */
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
 
 /***********************************************************/
 
@@ -28,17 +29,16 @@ cbuffer PS_VARIABLES : register(b0)
     float _Mixing;
     float _Border;
     bool __;
-	bool _Is_Pre_296_Build;
-	bool ___;
+
 };
 
 struct PS_INPUT
 {
-  float4 Tint : COLOR0;
-  float2 texCoord : TEXCOORD0;
-  float2 bgCoord : TEXCOORD1;
+    float4 Tint : COLOR0;
+    float2 texCoord : TEXCOORD0;
+	float2 bgCoord : TEXCOORD1;
+    float4 Position : SV_POSITION;
 };
-
 
 struct PS_OUTPUT
 {
@@ -55,129 +55,88 @@ cbuffer PS_PIXELSIZE : register(b1)
 /* Main */
 /************************************************************/
 
-float Fun_Lum (float4 _Result) { return (0.2126 * _Result.r + 0.7152 * _Result.g + 0.0722 * _Result.b) * _Result.a; }
+static const int SAMPLES_INNER  = 12;
+#define PIXELSIZE               float2(fPixelWidth, fPixelHeight)
 
-float Fun_OuterOutline(float2 In, float _Tint)
+float Fun_Inner(float2 In)
 {
-    float2 _PX = float2(fPixelWidth, fPixelHeight);
-    float _Alpha = S2D_Image.Sample(S2D_ImageSampler, In).a * _Tint;
+    float _Alpha = 0.0;
+    for(int y = 0; y <= SAMPLES_INNER; y++)
+    {
+        for(int x = 0; x <= SAMPLES_INNER; x++)
+        {
+            float2 _Offset = (float2(x, y) / (float)SAMPLES_INNER - 0.5);
+            float _Render = S2D_Image.Sample(S2D_ImageSampler, In + _Offset * _Border * PIXELSIZE).a;
 
-    float aL = S2D_Image.Sample(S2D_ImageSampler, In + float2(-_PX.x * _Border, 0.0)).a * _Tint;
-    float aR = S2D_Image.Sample(S2D_ImageSampler, In + float2( _PX.x * _Border, 0.0)).a * _Tint;
-    float aU = S2D_Image.Sample(S2D_ImageSampler, In + float2(0.0, -_PX.y * _Border)).a * _Tint;
-    float aD = S2D_Image.Sample(S2D_ImageSampler, In + float2(0.0,  _PX.y * _Border)).a * _Tint;
-
-    return step(0.01, aL + aR + aU + aD) * (1.0 - _Alpha) * ( (aL + aR + aU + aD));
+            _Alpha += _Render;
+        }
+    }
+    return _Alpha / float(SAMPLES_INNER * SAMPLES_INNER);
 }
 
-float3 Fun_Outline(float2 In, float3 _Color, float _Tint)
-{
-    float2 _PX = float2(fPixelWidth, fPixelHeight);
-    float _Alpha = S2D_Image.Sample(S2D_ImageSampler, In).a * _Tint;
-
-    float aL1 = S2D_Image.Sample(S2D_ImageSampler, In + float2(-_PX.x * _Border, 0.0)).a * _Tint;
-    float aR1 = S2D_Image.Sample(S2D_ImageSampler, In + float2( _PX.x * _Border, 0.0)).a * _Tint;
-    float aU1 = S2D_Image.Sample(S2D_ImageSampler, In + float2(0.0, -_PX.y * _Border)).a * _Tint;
-    float aD1 = S2D_Image.Sample(S2D_ImageSampler, In + float2(0.0,  _PX.y * _Border)).a * _Tint;
-
-    float aL2 = S2D_Image.Sample(S2D_ImageSampler, In + float2(-_PX.x * 2.0 * _Border, 0.0)).a * _Tint;
-    float aR2 = S2D_Image.Sample(S2D_ImageSampler, In + float2( _PX.x * 2.0 * _Border, 0.0)).a * _Tint;
-    float aU2 = S2D_Image.Sample(S2D_ImageSampler, In + float2(0.0, -_PX.y * 2.0 * _Border)).a * _Tint;
-    float aD2 = S2D_Image.Sample(S2D_ImageSampler, In + float2(0.0,  _PX.y * 2.0 * _Border)).a * _Tint;
-
-        float _Edge1  = step(0.01, abs(aL1 - _Alpha) + abs(aR1 - _Alpha) + abs(aU1 - _Alpha) + abs(aD1 - _Alpha));
-        float _Edge2 = step(0.01, abs(aL2 - _Alpha) + abs(aR2 - _Alpha) + abs(aU2 - _Alpha) + abs(aD2 - _Alpha));
-
-    return lerp(_Color * 0.5, float3(1.0, 1.0, 1.0), _Edge1 * _Alpha) + _Edge2 * 0.25;
+float Fun_Lum (float4 _Result) { 
+    return (0.2126 * _Result.r + 0.7152 * _Result.g + 0.0722 * _Result.b) * _Result.a;
 }
 
-float Fun_Random(float2 In)
-{
+float Fun_Random(float2 In) {
     return frac(sin(dot(In, float2(12.9898, 78.233))) * 43758.5453);
 }
 
-
-PS_OUTPUT ps_main( in PS_INPUT In )
+float4 Demultiply(float4 _Render, bool _Premultiplied)
 {
-    PS_OUTPUT Out;
+    if(_Premultiplied)
+    {
+	    if ( _Render.a != 0.0 ) {
+            _Render.rgb /= _Render.a;
+        }
+    }
 
-    float4 _Render_Texture = S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint;
-    float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord);
+	return _Render;
+}
+
+float4 Main(in PS_INPUT In, bool _Premultiplied) : SV_TARGET
+{
+    float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint, _Premultiplied);
+    float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord * 1.0);
 
     float _Rand = Fun_Random(In.texCoord + _Render_Texture.rb + _Render_Texture.bg + _Render_Background.rg + _Render_Background.br);
+    float _Lum = Fun_Lum(_Render_Texture);
+    float _Lum_Background = Fun_Lum(_Render_Background);
 
-            float _Lum = Fun_Lum(_Render_Texture);
-            float _Lum_Background = Fun_Lum(_Render_Background);
+        float3 _Space = lerp(float3(0.0, 0.0, 0.0), float3(0.15, 0.05, 0.25), _Lum);
+        _Space += (_Space * (S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord + tan(_Lum) * _Lum * 0.25 - tan(_Lum_Background) * 0.25 - _Rand).rgb) * 1.5);
+        _Space *= 0.5;
 
-            float3 _Space = lerp(float3(0.0, 0.0, 0.0), float3(0.15, 0.05, 0.25), _Lum);
-            _Space += (_Space * (S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord + tan(_Lum) * _Lum * 0.25 - tan(_Lum_Background) * 0.25 - _Rand).rgb) * 1.5);
-            _Space *= 0.5;
-
-                float _Render_Background_Void_R = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord + (_Render_Background.r * (1.0 - _Render_Texture.r) + _Rand) * 0.1).r;
-                float _Render_Background_Void_G = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord + (_Render_Background.g * (1.0 - _Render_Texture.g) + _Rand) * 0.1).b;
-                float _Render_Background_Void_B = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord + (_Render_Background.b * (1.0 - _Render_Texture.b) + _Rand) * 0.1).b;
-
-                _Space = lerp(_Space, _Space + float3(_Render_Background_Void_R, _Render_Background_Void_G, _Render_Background_Void_B) * float3(0.15, 0.05, 0.25), 1.0 - _Lum);
-
+            float3 _Void = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord + (_Render_Background.rg * (1.0 - _Render_Texture.rg) + _Rand) * 0.1).rgb;
+            _Space = lerp(_Space, _Space + _Void * float3(0.15, 0.05, 0.25), 1.0 - _Lum);
 
             float4 _Result = _Render_Texture;
-            _Result.rgb = lerp(_Render_Texture.rgb, _Space + pow(Fun_Random(In.texCoord + _Space.rb + _Space.bg + _Render_Background.rg + _Render_Background.br + _Rand), 255.0), _Mixing);
-            _Result.rgb += (Fun_Outline(In.texCoord, _Space, In.Tint.a) + pow(_Lum, 6.0)) * saturate(_Mixing);
+            _Result.rgb = lerp( _Render_Texture.rgb, 
+                                _Space + pow(Fun_Random(In.texCoord + _Space.rb + _Space.bg + _Render_Background.rg + _Render_Background.br + _Rand), 255.0), 
+                                _Mixing);
 
-            float3 _Cosmos = pow(Fun_Random(In.texCoord), 128.0) * 0.5;
-            _Result = lerp(float4(_Cosmos.rgb, 1.0), _Result, 1.0 - pow(Fun_OuterOutline(In.texCoord, In.Tint.a), 4.0));
+            float _Inner = (1.0 - Fun_Inner(In.texCoord)) * _Render_Texture.a;
+            _Result.rgb += ((_Inner / pow(cos(_Lum - _Inner), 6.0)) * saturate(_Mixing)) * 0.05;
+            _Result.rgb += saturate(_Inner * _Inner + (1.0 - _Lum) * 0.15) * 4.0;
 
-            _Result = lerp(_Render_Texture, _Result, _Mixing);
+        _Result = lerp(_Render_Texture, _Result, _Mixing);
 
-    Out.Color = _Result;
-    
-    return Out;
+    return _Result;
 }
 
 /************************************************************/
-/* Premultiplied Alpha */
+/* Render */
 /************************************************************/
 
-float4 Demultiply(float4 _Color)
-{
-	if ( _Color.a != 0 )   _Color.rgb /= _Color.a;
-	return _Color;
+float4 ps_main(in PS_INPUT In) : SV_TARGET { 
+    float4 _Render = Main(In, false);
+    return _Render;
 }
 
-PS_OUTPUT ps_main_pm( in PS_INPUT In ) 
+float4 ps_main_pm(in PS_INPUT In) : SV_TARGET
 {
-    PS_OUTPUT Out;
+    float4 _Render = Main(In, true);
+    _Render.rgb *= _Render.a;
 
-    float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord)) * In.Tint;
-    float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord);
-
-    float _Rand = Fun_Random(In.texCoord + _Render_Texture.rb + _Render_Texture.bg + _Render_Background.rg + _Render_Background.br);
-
-            float _Lum = Fun_Lum(_Render_Texture);
-            float _Lum_Background = Fun_Lum(_Render_Background);
-
-            float3 _Space = lerp(float3(0.0, 0.0, 0.0), float3(0.15, 0.05, 0.25), _Lum);
-            _Space += (_Space * (S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord + tan(_Lum) * _Lum * 0.25 - tan(_Lum_Background) * 0.25 - _Rand).rgb) * 1.5);
-            _Space *= 0.5;
-
-                float _Render_Background_Void_R = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord + (_Render_Background.r * (1.0 - _Render_Texture.r) + _Rand) * 0.1).r;
-                float _Render_Background_Void_G = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord + (_Render_Background.g * (1.0 - _Render_Texture.g) + _Rand) * 0.1).b;
-                float _Render_Background_Void_B = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord + (_Render_Background.b * (1.0 - _Render_Texture.b) + _Rand) * 0.1).b;
-
-                _Space = lerp(_Space, _Space + float3(_Render_Background_Void_R, _Render_Background_Void_G, _Render_Background_Void_B) * float3(0.15, 0.05, 0.25), 1.0 - _Lum);
-
-
-            float4 _Result = _Render_Texture;
-            _Result.rgb = lerp(_Render_Texture.rgb, _Space + pow(Fun_Random(In.texCoord + _Space.rb + _Space.bg + _Render_Background.rg + _Render_Background.br + _Rand), 255.0), _Mixing);
-            _Result.rgb += (Fun_Outline(In.texCoord, _Space, In.Tint.a) + pow(_Lum, 6.0)) * saturate(_Mixing);
-
-            float3 _Cosmos = pow(Fun_Random(In.texCoord), 128.0) * 0.5;
-            _Result = lerp(float4(_Cosmos.rgb, 1.0), _Result, 1.0 - pow(Fun_OuterOutline(In.texCoord, In.Tint.a), 4.0));
-
-            _Result = lerp(_Render_Texture, _Result, _Mixing);
-
-    _Result.rgb *= _Result.a;
-
-    Out.Color = _Result;
-    return Out;
+    return _Render;
 }
