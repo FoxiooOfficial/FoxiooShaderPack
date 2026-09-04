7fb77ddd57c3f2706b1c1cf1cf48ab7a6616ccf1
Foxioo <foxioo2137@gmail.com>
2026-09-04 23:29:16 +0200

    another new shaders

--- Foxioo Shader Pack/Special/Dithering/Commodore/Commodore 16 16-Color (Switch).hlsl
+++ Foxioo Shader Pack/Special/Dithering/Commodore/Commodore 16 16-Color (Switch).hlsl
@@ -13,17 +13,45 @@
 /* Samplers */
 /***********************************************************/
 
-sampler2D S2D_Image : register(s0);
-sampler2D S2D_Background : register(s1);
+Texture2D<float4> S2D_Image : register(t0);
+SamplerState S2D_ImageSampler : register(s0);
+
+Texture2D<float4> S2D_Background : register(t1);
+SamplerState S2D_BackgroundSampler : register(s1);
 
 /***********************************************************/
 /* Variables */
 /***********************************************************/
 
-    float   _Mixing, _Add, _Mul, _DitheringSize,
-            fPixelWidth, fPixelHeight;
+cbuffer PS_VARIABLES : register(b0)
+{
+    bool _;
+    bool _Blending_Mode;
+    float _Mixing;
+    float _DitheringSize; 
+    float _Add;
+    float _Mul;
+    bool __;
+};
 
-    bool    _Blending_Mode;
+struct PS_INPUT
+{
+    float4 Tint : COLOR0;
+    float2 texCoord : TEXCOORD0;
+	float2 bgCoord : TEXCOORD1;
+    float4 Position : SV_POSITION;
+};
+
+struct PS_OUTPUT
+{
+    float4 Color   : SV_TARGET;
+};
+
+cbuffer PS_PIXELSIZE : register(b1)
+{
+	float fPixelWidth;
+	float fPixelHeight;
+};
 
 /************************************************************/
 /* Main */
@@ -33,22 +61,22 @@ sampler2D S2D_Background : register(s1);
 
 static const float3 _Palette[_Palette_Size] = 
 {
-    float3(0.87, 0.87, 0.87),
-    float3(0.74, 0.74, 0.74),
-    float3(0.6, 0.6, 0.6),
-    float3(0.47, 0.47, 0.47),
-    float3(0.33, 0.33, 0.33),
-    float3(0.19, 0.19, 0.19),
-    float3(0.0, 0.27, 0.59),
-    float3(0.95, 0.94, 0.22),
-    float3(0.21, 0.8, 0.18),
-    float3(0.85, 0.0, 0.0),
-    float3(0.95, 0.94, 0.75),
-    float3(0.35, 0.54, 0.11),
-    float3(0.99, 0.73, 0.17),
-    float3(0.13, 0.75, 0.99),
     float3(0.0, 0.0, 0.0),
     float3(1.0, 1.0, 1.0),
+    float3(0.62, 0.16, 0.15),
+    float3(0.34, 0.76, 0.8),
+    float3(0.61, 0.19, 0.68),
+    float3(0.3, 0.69, 0.24),
+    float3(0.2, 0.16, 0.98),
+    float3(0.76, 0.92, 0.22),
+    float3(0.62, 0.3, 0.09),
+    float3(0.38, 0.26, 0.04),
+    float3(0.81, 0.36, 0.33),
+    float3(0.31, 0.31, 0.31),
+    float3(0.47, 0.47, 0.47),
+    float3(0.54, 0.94, 0.47),
+    float3(0.45, 0.32, 0.98),
+    float3(0.62, 0.62, 0.62)
 };
 
 static const float _Dithering[16] =
@@ -67,10 +95,22 @@ float3 Fun_Convert(float3 _Color)
         return lerp(_High, _Low, step(_Color, float3(0.04045, 0.04045, 0.04045)));
 }
 
-float4 ps_main(in float2 In : TEXCOORD0, in float2 In_Background : TEXCOORD1) : COLOR0
+float4 Demultiply(float4 _Render, bool _Premultiplied)
 {
-    float4 _Render_Texture = tex2D(S2D_Image, In);
-    float4 _Render_Background = tex2D(S2D_Background, In_Background);
+    if(_Premultiplied)
+    {
+	    if ( _Render.a != 0.0 ) {
+            _Render.rgb /= _Render.a;
+        }
+    }
+
+	return _Render;
+}
+
+float4 Main(in PS_INPUT In, bool _Premultiplied) : SV_TARGET
+{
+    float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint, _Premultiplied);
+    float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord);
 
     _Render_Texture.rgb = _Render_Texture.rgb * _Mul + _Add;
     _Render_Background.rgb = _Render_Background.rgb * _Mul + _Add;
@@ -88,39 +128,51 @@ float4 ps_main(in float2 In : TEXCOORD0, in float2 In_Background : TEXCOORD1) : 
             _Render = _Render_Background;
         }
 
-        int2 _Dith = int2(fmod(In / float2(fPixelWidth, fPixelHeight), 4.0));
+        int2 _Dith = int2(  fmod(In.texCoord.x / fPixelWidth,   4.0), 
+                            fmod(In.texCoord.y / fPixelHeight,  4.0)
+                        );
 
         int _Index = _Dith.x + _Dith.y * 4;
         float _DithValue = _Dithering[_Index];
                 
-            float3 _Color = _Result.rgb + (_DithValue - 0.5) * _DitheringSize;
+        float3 _Color = _Result.rgb + (_DithValue - 0.5) * _DitheringSize;
                 
             float _MinDist = 1e9;
             int _IndexC = 0;
-            
+                
+            for (int i = 0; i < _Palette_Size; i++)
+            {
                 float3 _PO = Fun_Convert(_Color);
-                for (int i = 0; i < _Palette_Size; i++)
+                float3 _PL = Fun_Convert(_Palette[i]);
+                    
+                float _Dist = distance(_PO, _PL);
+                    
+                if (_Dist < _MinDist)
                 {
-                    float3 _PL = Fun_Convert(_Palette[i]);
-                    float3 _Diff = _PO - _PL;
-                    float _Dist = dot(_Diff, _Diff);
-                        
-                    if (_Dist < _MinDist)
-                    {
-                        _MinDist = _Dist;
-                        _IndexC = i;
-                    }
+                    _MinDist = _Dist;
+                    _IndexC = i;
                 }
+            }
 
         _Result.rgb = _Palette[_IndexC];
         _Result.rgb = lerp(_Render.rgb, _Result.rgb, _Mixing); 
-
 
     return _Result;
 }
 
 /************************************************************/
-/* Tech Main */
+/* Render */
 /************************************************************/
 
-technique tech_main { pass P0 { PixelShader = compile ps_3_0 ps_main(); } }
+float4 ps_main(in PS_INPUT In) : SV_TARGET { 
+    float4 _Render = Main(In, false);
+    return _Render;
+}
+
+float4 ps_main_pm(in PS_INPUT In) : SV_TARGET
+{
+    float4 _Render = Main(In, true);
+    _Render.rgb *= _Render.a;
+
+    return _Render;
+}
