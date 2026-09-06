0d8a58fefca9bd04b04e8dc2555e48f67e85bc33
Foxioo <foxioo2137@gmail.com>
2026-09-06 13:26:40 +0200

    fxwg port for gi natlan outline

--- Foxioo Shader Pack/Special/Outline/Recreations/Genshin Impact/Genshin Impact Natlan Outline (Background).fx
+++ Foxioo Shader Pack/Special/Outline/Recreations/Genshin Impact/Genshin Impact Natlan Outline (Background).fx
@@ -1,8 +1,9 @@
 /***********************************************************/
 
-/* Shader author: Foxioo */
-/* Version shader: 1.1 (18.10.2025) */
-/* My GitHub: https://github.com/FoxiooOfficial */
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
 
 /***********************************************************/
 
@@ -37,16 +38,69 @@ sampler2D S2D_Background : register(s1);
             _Color, 
             _ColorLight;
 
+    float4x4    transformMatrix,
+                projectionMatrix; 
+
+struct VS_INPUT
+{
+    float4 Tint     : COLOR0;
+    float2 texCoord : TEXCOORD0;
+    float2 bgCoord  : TEXCOORD1;
+    float4 Position : POSITION;
+};
+
+struct VS_OUTPUT
+{
+    float4 Tint     : COLOR0;
+    float2 texCoord : TEXCOORD0;
+    float2 bgCoord  : TEXCOORD1;
+    float4 Position : POSITION;
+};
+
+/************************************************************/
+/* Vertex Shader */
+/************************************************************/
+
+VS_OUTPUT vs_main(VS_INPUT In)
+{
+	VS_OUTPUT Out;
+
+	float2 _PixelSize = float2(fPixelWidth, fPixelHeight);
+	float2 _DirCorner = sign(In.texCoord - 0.5);
+
+        float _Expanded = abs(_Offset + _OffsetDistortion);
+        float2 _PixelPadding = _Expanded * float2(fPixelWidth, fPixelHeight);
+        float4 _PosExpanded = In.Position;
+
+            _PosExpanded.xy += _DirCorner * _Expanded;
+
+	Out.Position = mul(_PosExpanded, transformMatrix);
+	Out.Position = mul(Out.Position, projectionMatrix);
+
+	Out.Tint = In.Tint;
+	Out.texCoord = In.texCoord + _DirCorner * _PixelPadding;
+    Out.bgCoord = In.bgCoord;
+
+	return Out;
+}
+
 /************************************************************/
 /* Main */
 /************************************************************/
+
+float4 Fun_Render(sampler2D _Tex, float2 In) {
+    if(any(In <= 0.0 || In >= 1.0))
+        return 0.0;
+    else
+        return tex2D(_Tex, In);
+}
 
 static const float2 _OffsetEx[8] = 
 {
     float2(-1.0,  0.0),
     float2( 1.0,  0.0),
-    float2( 0.00001, -1.0),
-    float2( 0.00001,  1.0),
+    float2( 0.0, -1.0),
+    float2( 0.0,  1.0),
 
     float2(-1.0, -1.0),
     float2(-1.0,  1.0),
@@ -54,10 +108,14 @@ static const float2 _OffsetEx[8] =
     float2( 1.0,  1.0) 
 };
 
-float Fun_Hash21(float2 _Pos) { return frac(sin(dot(_Pos, float2(12.9898,78.233))) * 43758.5453); }
+float Fun_Hash21(float2 _Pos) { 
+    return frac(sin(dot(_Pos, float2(12.9898,78.233))) * 43758.5453);
+}
+
 float Fun_Noise(float2 _Pos)
 {
-    float2 _I = floor(_Pos + _Seed);    float2 _F = frac(_Pos);
+    float2 _I = floor(_Pos + _Seed);
+    float2 _F = frac(_Pos);
 
         float _A = Fun_Hash21(_I + float2(0.0, 0.0) + _Seed);
         float _B = Fun_Hash21(_I + float2(1.0, 0.0) + _Seed);
@@ -86,31 +144,33 @@ float3 Fun_NoiseSat(float3 _Color, float _Sat)
     return lerp(float3(_Lum, _Lum, _Lum), _Color, _Sat);
 }
 
-float4 ps_main(in float2 In : TEXCOORD0, in float2 In_Background : TEXCOORD1) : COLOR0
+float4 ps_main(VS_INPUT In) : COLOR0
 {
-    float4 _Render_Texture = tex2D(S2D_Image, In);
-    float4 _Render_Background = tex2D(S2D_Background, In_Background);
+    float4 _Render_Texture = Fun_Render(S2D_Image, In.texCoord);
+    float4 _Render_Background = Fun_Render(S2D_Background, In.bgCoord);
 
-        float2 _UV = (In + float2(_PosX, _PosY)) * float2(_ScaleX, _ScaleY) * _Scale; 
+        float2 _UV = (In.texCoord + float2(_PosX, _PosY)) * float2(_ScaleX, _ScaleY) * _Scale; 
         float4 _Result = _Color;
 
             _Result.rgb = Fun_NoiseGradient(_UV, _Color.rgb, _ColorShadow.rgb, _ColorLight.rgb);
 
-                float _Noise = Fun_Noise(_UV * 8.0);     float _Sat = lerp(0.5, 2.5, _Noise);
+                float _Noise = Fun_Noise(_UV * 8.0);
+                float _Sat = lerp(0.5, 2.5, _Noise);
+
                 _Result.rgb = Fun_NoiseSat(_Result.rgb, _Sat);
 
                     float _Mask = _Render_Texture.a;
 
                     for (int i = 0; i < 8; i++)
                     {
-                        _Mask += tex2D(S2D_Image, In + _OffsetEx[i] * float2(fPixelWidth, fPixelHeight) * (_Offset + sin(In + _Noise) * _OffsetDistortion)).a;
+                        _Mask += Fun_Render(S2D_Image, In.texCoord + _OffsetEx[i] * float2(fPixelWidth, fPixelHeight) * (_Offset + sin(In.texCoord + _Noise) * _OffsetDistortion)).a;
                         _Mask = saturate(_Mask);
                     }
                 
                     _Result.a = _Mask;
 
-                _Result.rgb = lerp(_Result.rgb, _Result.rgb + _Render_Background.rgb, _Mixing);
-                float4 _Render = lerp(_Result, _Render_Texture, _Render_Texture.a * _Alpha);
+        _Result.rgb = lerp(_Result.rgb, _Result.rgb + _Render_Background.rgb, _Mixing);
+        float4 _Render = lerp(_Result, _Render_Texture, _Render_Texture.a * _Alpha);
 
     return _Render;
 }
@@ -119,4 +179,11 @@ float4 ps_main(in float2 In : TEXCOORD0, in float2 In_Background : TEXCOORD1) : 
 /* Tech Main */
 /************************************************************/
 
-technique tech_main { pass P0 { PixelShader = compile ps_2_a ps_main(); } }
+technique tech_main
+{
+	pass P0
+	{
+		VertexShader = compile vs_1_1 vs_main();
+		PixelShader = compile ps_2_a ps_main();
+	}
+}
--- Foxioo Shader Pack/Special/Outline/Recreations/Genshin Impact/Genshin Impact Natlan Outline (Background).hlsl
+++ Foxioo Shader Pack/Special/Outline/Recreations/Genshin Impact/Genshin Impact Natlan Outline (Background).hlsl
@@ -1,8 +1,9 @@
 /***********************************************************/
 
-/* Shader author: Foxioo */
-/* Version shader: 1.1 (18.10.2025) */
-/* My GitHub: https://github.com/FoxiooOfficial */
+/* Copyright (c) 2024-2026 Foxioo */
+/* Project repository page: https://github.com/FoxiooOfficial/FoxiooShaderPack */
+/* MIT License; for more details, see: https://github.com/FoxiooOfficial/FoxiooShaderPack/blob/main/LICENSE */
+/* Information about the shader version can be found in the effect's .xml file */
 
 /***********************************************************/
 
@@ -22,59 +23,156 @@ SamplerState S2D_BackgroundSampler : register(s1);
 /* Variables */
 /***********************************************************/
 
-cbuffer PS_VARIABLES : register(b0)
-{
-    bool _;
-    bool __;
-    float _PosX;
-    float _PosY;    
-    bool ___;
-    float _Scale;
-    float _ScaleX;
-    float _ScaleY;
-    bool ____;
-    float _Mixing;
-    float _Alpha;
-    float _Offset;
-    float _OffsetDistortion;
-    float _Seed;
-    float4 _ColorLight; 
-    float4 _Color; 
-    float4 _ColorShadow; 
-    bool _____;
-	bool _Is_Pre_296_Build;
-    bool ______;
-};
-
 struct PS_INPUT
 {
-  float4 Tint : COLOR0;
-  float2 texCoord : TEXCOORD0;
-  float2 bgCoord : TEXCOORD1;
+    float4 Tint : COLOR0;
+    float2 texCoord : TEXCOORD0;
+	float2 bgCoord : TEXCOORD1;
+    float4 Position : SV_POSITION;
 };
 
+#ifdef FUSION_PIXEL_SHADER
 
-struct PS_OUTPUT
+    cbuffer PS_VARIABLES : register(b0)
+    {
+        bool _;
+        bool __;
+        float _PosX;
+        float _PosY;    
+        bool ___;
+        float _Scale;
+        float _ScaleX;
+        float _ScaleY;
+        bool ____;
+        float _Mixing;
+        float _Alpha;
+        float _Offset;
+        float _OffsetDistortion;
+        float _Seed;
+        float4 _ColorLight; 
+        float4 _Color; 
+        float4 _ColorShadow; 
+        bool _____;
+    };
+
+    cbuffer PS_PIXELSIZE : register(b1)
+    {
+        float fPixelWidth;
+        float fPixelHeight;
+    };
+
+#endif // FUSION_PIXEL_SHADER
+
+/***********************************************************/
+
+struct VS_INPUT
 {
-    float4 Color   : SV_TARGET;
+    float4 Tint     : COLOR0;
+    float2 texCoord : TEXCOORD0;
+	float2 bgCoord : TEXCOORD1;
+    float3 Position : SV_POSITION;
 };
 
-cbuffer PS_PIXELSIZE : register(b1)
+#ifdef FUSION_VERTEX_SHADER
+
+	cbuffer VS_MATRICES : register(b0)
+	{
+		row_major float4x4 transformMatrix;
+		row_major float4x4 projectionMatrix;
+	};
+
+    cbuffer VS_VARIABLES : register(b1)
+    {
+        bool _;
+        bool __;
+        float _PosX;
+        float _PosY;    
+        bool ___;
+        float _Scale;
+        float _ScaleX;
+        float _ScaleY;
+        bool ____;
+        float _Mixing;
+        float _Alpha;
+        float _Offset;
+        float _OffsetDistortion;
+        float _Seed;
+        float4 _ColorLight; 
+        float4 _Color; 
+        float4 _ColorShadow; 
+        bool _____;
+    };
+
+    cbuffer VS_PIXELSIZE : register(b2)
+    {
+        float fPixelWidth;
+        float fPixelHeight;
+    };
+
+#endif // FUSION_VERTEX_SHADER
+
+/************************************************************/
+/* Vertex Shader */
+/************************************************************/
+
+#ifdef FUSION_VERTEX_SHADER
+
+// fusion-fx-preview: allow-fxc-warnings
+PS_INPUT vs_main(VS_INPUT In)
 {
-	float fPixelWidth;
-	float fPixelHeight;
-};
+	PS_INPUT Out;
+
+	float2 _PixelSize = float2(fPixelWidth, fPixelHeight);
+	float2 _DirCorner = sign(In.texCoord - 0.5);
+
+        float _Expanded = abs(_Offset + _OffsetDistortion);
+        float2 _PixelPadding = _Expanded * float2(fPixelWidth, fPixelHeight);
+        float4 _PosExpanded = float4(In.Position, 1.0);
+
+            _PosExpanded.xy += _DirCorner * _Expanded;
+
+	Out.Position = mul(_PosExpanded, transformMatrix);
+	Out.Position = mul(Out.Position, projectionMatrix);
+
+	Out.Tint = In.Tint;
+	Out.texCoord = In.texCoord + _DirCorner * _PixelPadding;
+    Out.bgCoord = In.bgCoord;
+
+	return Out;
+}
+
+#endif // FUSION_VERTEX_SHADER
 
 /************************************************************/
 /* Main */
 /************************************************************/
 
+#ifdef FUSION_PIXEL_SHADER
+
+float4 Fun_Render(Texture2D _Tex, SamplerState _TexSampler, float2 In) {
+    if(any(In <= 0.0 || In >= 1.0))
+        return 0.0;
+    else
+        return _Tex.Sample(_TexSampler, In);
+}
+
+float4 Demultiply(float4 _Render, bool _Premultiplied)
+{
+    if(_Premultiplied)
+    {
+        if ( _Render.a != 0.0 ) {
+            _Render.rgb /= _Render.a;
+        }
+    }
+    return _Render;
+}
+
 static const float2 _OffsetEx[8] = 
 {
     float2(-1.0,  0.0),
     float2( 1.0,  0.0),
-    float2( 0.00001, -1.0),
-    float2( 0.00001,  1.0),
+    float2( 0.0, -1.0),
+    float2( 0.0,  1.0),
 
     float2(-1.0, -1.0),
     float2(-1.0,  1.0),
@@ -82,10 +180,14 @@ static const float2 _OffsetEx[8] =
     float2( 1.0,  1.0) 
 };
 
-float Fun_Hash21(float2 _Pos) { return frac(sin(dot(_Pos, float2(12.9898,78.233))) * 43758.5453); }
+float Fun_Hash21(float2 _Pos) { 
+    return frac(sin(dot(_Pos, float2(12.9898,78.233))) * 43758.5453);
+}
+
 float Fun_Noise(float2 _Pos)
 {
-    float2 _I = floor(_Pos + _Seed);    float2 _F = frac(_Pos);
+    float2 _I = floor(_Pos + _Seed);
+    float2 _F = frac(_Pos);
 
         float _A = Fun_Hash21(_I + float2(0.0, 0.0) + _Seed);
         float _B = Fun_Hash21(_I + float2(1.0, 0.0) + _Seed);
@@ -114,79 +216,52 @@ float3 Fun_NoiseSat(float3 _Color, float _Sat)
     return lerp(float3(_Lum, _Lum, _Lum), _Color, _Sat);
 }
 
-PS_OUTPUT ps_main( in PS_INPUT In )
+float4 Main(PS_INPUT In, bool _Premultiplied) : SV_TARGET
 {
-    PS_OUTPUT Out;
-
-    float4 _Render_Texture = S2D_Image.Sample(S2D_ImageSampler, In.texCoord) * In.Tint;
-    float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord);
+    float4 _Render_Texture = Demultiply(Fun_Render(S2D_Image, S2D_ImageSampler, In.texCoord) * In.Tint, _Premultiplied);
+    float4 _Render_Background = Fun_Render(S2D_Background, S2D_BackgroundSampler, In.bgCoord);
 
         float2 _UV = (In.texCoord + float2(_PosX, _PosY)) * float2(_ScaleX, _ScaleY) * _Scale; 
         float4 _Result = _Color;
 
             _Result.rgb = Fun_NoiseGradient(_UV, _Color.rgb, _ColorShadow.rgb, _ColorLight.rgb);
 
-                float _Noise = Fun_Noise(_UV * 8.0);     float _Sat = lerp(0.5, 2.5, _Noise);
+                float _Noise = Fun_Noise(_UV * 8.0);
+                float _Sat = lerp(0.5, 2.5, _Noise);
+
                 _Result.rgb = Fun_NoiseSat(_Result.rgb, _Sat);
 
                     float _Mask = _Render_Texture.a;
 
                     for (int i = 0; i < 8; i++)
                     {
-                        _Mask += S2D_Image.Sample(S2D_ImageSampler, In.texCoord + _OffsetEx[i] * float2(fPixelWidth, fPixelHeight) * (_Offset + sin(In.texCoord + _Noise) * _OffsetDistortion)).a * In.Tint.a;
+                        _Mask += Fun_Render(S2D_Image, S2D_ImageSampler, In.texCoord + _OffsetEx[i] * float2(fPixelWidth, fPixelHeight) * (_Offset + sin(In.texCoord + _Noise) * _OffsetDistortion)).a * In.Tint.a;
                         _Mask = saturate(_Mask);
                     }
                 
                     _Result.a = _Mask;
 
-                _Result.rgb = lerp(_Result.rgb, _Result.rgb + _Render_Background.rgb, _Mixing);
-                float4 _Render = lerp(_Result, _Render_Texture, _Render_Texture.a * _Alpha);
+        _Result.rgb = lerp(_Result.rgb, _Result.rgb + _Render_Background.rgb, _Mixing);
+        float4 _Render = lerp(_Result, _Render_Texture, _Render_Texture.a * _Alpha);
 
-    Out.Color = _Render;
-    
-    return Out;
+	return _Render;
 }
 
 /************************************************************/
-/* Premultiplied Alpha */
+/* Render */
 /************************************************************/
 
-float4 Demultiply(float4 _Color)
-{
-	if ( _Color.a != 0 )   _Color.rgb /= _Color.a;
-	return _Color;
+float4 ps_main(PS_INPUT In) : SV_TARGET{
+    float4 _Render = Main(In, false);
+    return _Render;
 }
 
-PS_OUTPUT ps_main_pm( in PS_INPUT In ) 
+float4 ps_main_pm(PS_INPUT In) : SV_TARGET
 {
-    PS_OUTPUT Out;
-
-    float4 _Render_Texture = Demultiply(S2D_Image.Sample(S2D_ImageSampler, In.texCoord)) * In.Tint;
-    float4 _Render_Background = S2D_Background.Sample(S2D_BackgroundSampler, In.bgCoord);
-
-        float2 _UV = (In.texCoord + float2(_PosX, _PosY)) * float2(_ScaleX, _ScaleY) * _Scale; 
-        float4 _Result = _Color;
-
-            _Result.rgb = Fun_NoiseGradient(_UV, _Color.rgb, _ColorShadow.rgb, _ColorLight.rgb);
-
-                float _Noise = Fun_Noise(_UV * 8.0);     float _Sat = lerp(0.5, 2.5, _Noise);
-                _Result.rgb = Fun_NoiseSat(_Result.rgb, _Sat);
-
-                    float _Mask = _Render_Texture.a;
-
-                    for (int i = 0; i < 8; i++)
-                    {
-                        _Mask += S2D_Image.Sample(S2D_ImageSampler, In.texCoord + _OffsetEx[i] * float2(fPixelWidth, fPixelHeight) * (_Offset + sin(In.texCoord + _Noise) * _OffsetDistortion)).a * In.Tint.a;
-                        _Mask = saturate(_Mask);
-                    }
-                
-                    _Result.a = _Mask;
-
-                _Result.rgb = lerp(_Result.rgb, _Result.rgb + _Render_Background.rgb, _Mixing);
-                float4 _Render = lerp(_Result, _Render_Texture, _Render_Texture.a * _Alpha);
-
+    float4 _Render = Main(In, true);
     _Render.rgb *= _Render.a;
 
-    Out.Color = _Render;
-    return Out;
+    return _Render;
 }
+
+#endif // FUSION_PIXEL_SHADER
